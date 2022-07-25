cff30eda0a8c8728f3964b914f7f44f77cc28f9e
ShundaZhang <shunda.zhang@intel.com>
2022-07-25 02:05:20 +0000

    Flagged Fibopadccils!

--- ctf/crypto/Fibopadcci/dec.py
+++ ctf/crypto/Fibopadcci/dec.py
@@ -21,7 +21,7 @@ def bitflip(c,buf,index,padding,io):
 		else:
 			print index,i
 
-ip,port = '138.68.190.245',30503
+ip,port = '206.189.21.202',31254
 #ip,port = '127.0.0.1',1337
 io = remote(ip, port)
 
@@ -35,13 +35,27 @@ buf = buf.split('\n')
 c = buf[0].split(' ')[2]
 a = buf[1].split(' ')[1]
 a2 = 'HTB{th3_s3crt_A}'
-iv = buf[2].split(' ')[1]
-c1 = c[:32]
+
+#block1
+#iv = buf[2].split(' ')[1]
+#c1 = c[:32]
+#c1 = xor(c1.decode('hex'),a.decode('hex'),a2).encode('hex')
+
+#block2
+#iv = c[:32]
+#c1 = c[32:64]
+#p1 = 'HTB{cU5t0m_p4dd1'
+#c1 = xor(c1.decode('hex'),p1,a2).encode('hex')
+
+#block3
+iv = c[32:64]
+c1 = c[64:96]
+p2 = 'Ng_w0nT_s4v3_y0u'
+c1 = xor(c1.decode('hex'),p2,a2).encode('hex')
 
 #print buf
 #print iv
 
-c1 = xor(c1.decode('hex'),a.decode('hex'),a2).encode('hex')
 padding = ''
 keystream = ''
 buf0 = '0'*32
@@ -72,4 +86,14 @@ print flag
 eb133da0d88ee2c02a5be2206cd6eff0
 a3477fdbbbdbd7b41a36bd5058b28bc1
 HTB{cU5t0m_p4dd1
+**************************************
+2ebbbe548d2712c5092b1d94f2b8ca8b
+60dce123bd49469a7a1f6ba7adc1fafe
+Ng_w0nT_s4v3_y0u
+**************************************
+5a94285b1e64cce05c4f73f2bbf05e92
+05e0606a6d3b98d1317c2cc893d11eef
+_tH1s_T1m3_:(!@}
+
+HTB{cU5t0m_p4dd1Ng_w0nT_s4v3_y0u_tH1s_T1m3_:(!@}
 '''
