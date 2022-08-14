053705af3b03ef96d6e8019b4dfb8a81d0988fb1
ShundaZhang <shunda.zhang@intel.com>
2022-08-14 11:20:36 +0000

    Guess vigenere key...

--- ctf/crypto/Classicyetcomplicated/dec.py
+++ ctf/crypto/Classicyetcomplicated/dec.py
@@ -3,3 +3,16 @@
 #https://www.boxentriq.com/code-breaking/vigenere-cipher
 #Auto Solve (without key)
 #The key is: helloworld
+
+'''
+Guess the algorithm is vigenere.
+Guess alp is the (duplicated twice).
+python
+>>> chr((ord('a')-ord('t')+26)%26+ord('a'))
+'h'
+>>> chr((ord('l')-ord('h')+26)%26+ord('a'))
+'e'
+>>> chr((ord('p')-ord('e')+26)%26+ord('a'))
+'l'
+Guess the key is helloworld...
+'''
