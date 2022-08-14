28cee4b7d764f7f5681c3adb1fd721f629a47e7f
ShundaZhang <shunda.zhang@intel.com>
2022-08-14 11:10:36 +0000

    Flagged Classic yet complicated.

---
+++ ctf/crypto/Classicyetcomplicated/dec.py
@@ -0,0 +1,5 @@
+#cipher identifier
+#https://www.boxentriq.com/code-breaking/cipher-identifier
+#https://www.boxentriq.com/code-breaking/vigenere-cipher
+#Auto Solve (without key)
+#The key is: helloworld
