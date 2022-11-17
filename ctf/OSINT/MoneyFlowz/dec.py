8758b5cd200031085528158ddccf950bcabe51c2
szhan21 <shunda.zhang@intel.com>
2022-11-17 14:45:40 +0800

    Flagged MoneyFlowz!

---
+++ ctf/OSINT/MoneyFlowz/dec.py
@@ -0,0 +1,20 @@
+'''
+Google Frank Vitalik
+
+Found a page in reddit: https://www.reddit.com/user/frankvitalik/
+disucssing crypto money flow...
+
+https://steemit.com/htb/@freecoinz/freecoinz
+0x1b3247Cd0A59ac8B37A922804D150556dB837699
+Wow! I can't believe they are giving free coins into the ropsten net!
+
+https://ropsten.etherscan.io/
+search 0x1b3247Cd0A59ac8B37A922804D150556dB837699
+
+Show all the transactions with 100 records per page, and can review from the last record.
+Found one transaction (out transaction) has addiontal input:
+https://ropsten.etherscan.io/tx/0xe1320c23f292e52090e423e5cdb7b4b10d3c70a8d1b947dff25ae892609f2ef4
+
+Show more -> Input Data: 0x4854427b43727950743043757272336e63795f31735f46754e7a21217d
+HTB{CryPt0Curr3ncy_1s_FuNz!!}
+'''
