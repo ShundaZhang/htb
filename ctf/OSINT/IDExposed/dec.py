f3eb4780ebb89ac22b6e958f8e48e007988ec9a1
szhan21 <shunda.zhang@intel.com>
2022-11-17 15:45:48 +0800

    Flagged ID Exposed!

---
+++ ctf/OSINT/IDExposed/dec.py
@@ -0,0 +1,13 @@
+'''
+Search location from Google ID
+
+https://www.linkedin.com/pulse/hack-box-id-exposed-write-up-charles-leerink/
+https://samanthactf.medium.com/hack-the-box-id-exposed-60a6034c4c19
+https://network47.org/htb-write-up-osint-id-exposed/
+
+1. Found Google ID from email address by google chat: https://hangouts.google.com
+2. Found the id from source html: 117395327982835488254
+3. Loacated in google map: google.com/maps/contrib/117395327982835488254
+
+In the reviews: HTB{i_W4S_D_I_S_c_O_v_3_R_3_D}
+'''
