9d390fc47c2ecf034ebf8f480d993a76c9cf2d15
szhan21 <shunda.zhang@intel.com>
2022-11-17 13:20:39 +0800

    Flagged Infiltration!

---
+++ ctf/OSINT/Infiltration/dec.py
@@ -0,0 +1,8 @@
+'''
+Google Evil Corp LLC
+Find a record of instagram
+https://www.instagram.com/p/BvbnFhTj9YS/
+It is a picture which show some secret of the company...
+
+HTB{Y0ur_Enum3rat10n_1s_Str0ng_Y0ung_0ne}
+'''
