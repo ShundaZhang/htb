ceac752ab9e08efeae880d21242e3f0ebf7b2b9c
szhan21 <shunda.zhang@intel.com>
2022-11-24 22:54:02 +0800

    Solved WrongSpookySeason.

---
+++ ctf/forensics/WrongSpookySeason/dec.py
@@ -0,0 +1,9 @@
+'''
+https://www.youtube.com/watch?v=4UzHNuhTJbs
+
+Statistics -> Protocol Hierarchy -> Data -> Apply as Filter -> Follow TCP Stream
+
+echo "==gC9FSI5tGMwA3cfRjd0o2Xz0GNjNjYfR3c1p2Xn5WMyBXNfRjd0o2eCRFS" | rev |base64 -d
+HTB{j4v4_5pr1ng_just_b3c4m3_j4v4_sp00ky!!}
+
+'''
