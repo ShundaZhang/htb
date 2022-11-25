19b914b89906e96b46bd0ec26e5686e5e0c021d1
szhan21 <shunda.zhang@intel.com>
2022-11-25 15:14:10 +0800

    Solved Chase.

---
+++ ctf/forensics/Chase/dec.py
@@ -0,0 +1,7 @@
+'''
+Check the protocol data or export the http objects.
+We can find a txt file, named JBKEE62NIFXF6ODMOUZV6NZTMFGV6URQMNMH2IBA.txt
+
+echo JBKEE62NIFXF6ODMOUZV6NZTMFGV6URQMNMH2IBA|base32 -d
+HTB{MAn_8lu3_73aM_R0cX}
+'''
