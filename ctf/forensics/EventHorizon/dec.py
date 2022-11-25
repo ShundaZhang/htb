d11d76e36e98d5cec6fe05602667d7f0545b7a2d
szhan21 <shunda.zhang@intel.com>
2022-11-25 22:25:56 +0800

    Solved Event Horizon.

---
+++ ctf/forensics/EventHorizon/dec.py
@@ -0,0 +1,10 @@
+'''
+https://josh-vr.medium.com/hackthebox-event-horizon-forensics-challenge-writeup-b32839a3307d
+
+Event Log Explorer.
+https://eventlogxp.com/download.php
+
+According to the description in the challenge, open Microsoft-Windows-PowerShell%4Operational.evtx
+Search HTB{
+HTB{8Lu3_734m_F0r3v3R}
+'''
