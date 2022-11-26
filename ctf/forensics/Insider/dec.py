38cdcfc55b6425f7864c739814ff0def7c9bcb1b
szhan21 <shunda.zhang@intel.com>
2022-11-26 10:26:54 +0800

    Solved Insider.

---
+++ ctf/forensics/Insider/dec.py
@@ -0,0 +1,14 @@
+'''
+Ubunut 20.04+, Python 3.9+
+
+git clone https://github.com/unode/firefox_decrypt.git
+python3.11 firefox_decrypt.py ~/htb/ctf/forensics/Insider/Mozilla/Firefox/
+Select the Mozilla profile you wish to decrypt
+1 -> Profiles/yodxf5e0.default
+2 -> Profiles/2542z9mo.default-release
+2
+
+Website:   http://acc01:8080
+Username: 'admin'
+Password: 'HTB{ur_8RoW53R_H157Ory}'
+'''
