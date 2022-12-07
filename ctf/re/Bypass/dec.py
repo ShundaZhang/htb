3446877e013afb7ad225e0ce0ec585756e37d974
szhan21 <shunda.zhang@intel.com>
2022-12-07 10:47:27 +0800

    Solved Bypass.

---
+++ ctf/re/Bypass/dec.py
@@ -0,0 +1,9 @@
+'''
+.Net reverse engineering: https://github.com/dnSpy/dnSpy/releases
+Just debug it
+
+Enter a username: 123
+Enter a password: 123
+Please Enter the secret Key: ThisIsAReallyReallySecureKeyButYouCanReadItFromSourceSoItSucks
+Nice here is the Flag:HTB{SuP3rC00lFL4g}
+'''
