908f79aca7611fca75ffe5d3b95e717a1440025d
szhan21 <shunda.zhang@intel.com>
2022-12-29 16:55:46 +0800

    Solved Tear Or Dear.

--- ctf/re/TearOrDear/dec.py
+++ ctf/re/TearOrDear/dec.py
@@ -1,7 +1,16 @@
 '''
+https://stixsecurity.com/?p=124
+https://buildfunthings.com/posts/reversing-tear-or-dear/
+
 .net code, dnSpy to de-complie
 
+Debug
 
+if (this.username == this.o && this.check1(s))
+{
+	MessageBox.Show("Correct!");
+	return;
+}
+
+HTB{piph:roiw!@#}
 '''
-
-
