1ca94648d4a9e3d970693c3f1082e381d52ef0c2
szhan21 <shunda.zhang@intel.com>
2023-03-28 14:39:50 +0800

    Solved cryptoconundrum!

---
+++ ctf/crypto/crypto_cryptoconundrum/flag.py
@@ -0,0 +1,10 @@
+#HT TB WT WI IT TH HD DI IG GR RA AP SV VY SI IT TI IS SH HA AR RD DE ER
+
+f = ''
+s = 'HT TB BW WI IT TH HD DI IG GR RA AP PH HS SI IT TI IS SH HA AR RD DE ER '
+for i in range(0, len(s), 3):
+	f += s[i]
+f += 'R'
+print f
+
+#HTB{WITHDIGRAPHSITISHARDER}
