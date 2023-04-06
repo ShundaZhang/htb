792943e949f5d8e03f96aa7a4f892ab68bdb4745
Zhang, Shunda <zhangshunda@gmail.com>
2023-04-06 00:19:34 +0000

    Added the comunication code for math nightmare.

--- ctf/misc/ANightmareOnMathStreet/dec.py
+++ ctf/misc/ANightmareOnMathStreet/dec.py
@@ -7,5 +7,7 @@ context.log_level = 'debug'
 ip, port = '139.59.174.176', 30781
 io = remote(ip, port)
 
-buf = io.recvuntil(']:')
-print(buf.split(' '))
+io.recvuntil(']: ')
+buf = io.recvline()
+print(buf.decode().split(' '))
+print(buf.decode().split(' ')[:-2])
