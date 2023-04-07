84c818a50e9953fbf2662c38aa134c80091ae176
szhan21 <shunda.zhang@intel.com>
2023-04-07 12:28:37 +0800

    Solved Signals!

---
+++ ctf/HW/Signals/dec.py
@@ -0,0 +1,7 @@
+#ISS MMSSTV SSTV
+#https://marcocampione.com/posts/202303-write-up-signals-htb/
+#https://oe5lxr.at/decode-sstv-with-mmsstv/
+
+#mmsstv + vlc player 
+
+#HTB{5l0w-5c4n_73l3v1510n_h4m_r4d10_h4ck3r}
