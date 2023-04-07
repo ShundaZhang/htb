505f440c13758261d1ee4d53eaa13a32301eaf3f
szhan21 <shunda.zhang@intel.com>
2023-04-07 21:05:54 +0800

    Solved Don't Overreact.

---
+++ ctf/mobile/DontOverreact/dec.py
@@ -0,0 +1,6 @@
+#jadx -> asset
+
+#__d(function(g,r,i,a,m,e,d){Object.defineProperty(e,"__esModule",{value:!0}),e.myConfig=void 0;var t={importantData:"baNaNa".toLowerCase(),apiUrl:'https://www.hackthebox.eu/',debug:'SFRCezIzbTQxbl9jNDFtXzRuZF9kMG43XzB2MzIyMzRjN30='};e.myConfig=t},400,[]);
+
+#echo SFRCezIzbTQxbl9jNDFtXzRuZF9kMG43XzB2MzIyMzRjN30=|base64 -d
+#HTB{23m41n_c41m_4nd_d0n7_0v32234c7}
