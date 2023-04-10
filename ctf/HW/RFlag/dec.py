f4f5f5b36073b5d20eae1e6bda33b9f9daf643c2
szhan21 <shunda.zhang@intel.com>
2023-04-10 13:41:09 +0800

    Solved RFlag!

---
+++ ctf/HW/RFlag/dec.py
@@ -0,0 +1,8 @@
+#https://marcocampione.com/posts/202303-write-up-rflag-htb/
+#apt-get install rtl-433
+#rtl_433 signal.cf32 -A
+
+flag = '2aaaaaaa0c4e4854427b52465f4834636b316e365f31735f6330306c2121217d'
+
+print flag.decode('hex')
+#HTB{RF_H4ck1n6_1s_c00l!!!}
