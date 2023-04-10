0b9b695c68d8077bb4d5fccc17db928e00728292
szhan21 <shunda.zhang@intel.com>
2023-04-10 13:07:14 +0800

    Solved Query!

---
+++ ctf/misc/Query/dec.py
@@ -0,0 +1,5 @@
+#https://hack-the-flag.koyeb.app/challenges/155
+#https://drive.google.com/file/d/1gVH1M4TCTfljHQPf4ukTnLaWFGVvrp1X/view
+#SFRCe2ZPdU5kX2NMNHIxdHlfaU5fMGJmVXNjYVQhb059
+
+#HTB{fOuNd_cL4r1ty_iN_0bfUscaT!oN}
