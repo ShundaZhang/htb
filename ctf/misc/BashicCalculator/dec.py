f259a1035e550d975a28cbcde24c5e775bca19f4
szhan21 <shunda.zhang@intel.com>
2023-04-10 13:03:56 +0800

    Solved BashicCalculator!

---
+++ ctf/misc/BashicCalculator/dec.py
@@ -0,0 +1,15 @@
+'''
+https://github.com/AlexDowneyJr/CTFs/blob/b467c6a9c238397f47867080db6412994e6a42cf/HackTheBox/Bashic%20Calculator%20CTF/README.md
+Use tab instead of space, otherwise it will be filtered!
+
+command)	)	#
+
+cat	/flag.txt)	)	#
+
+OR
+
+{cat,/flag.txt})<<\
+
+
+HTB{Ju4nck3r_15_y0ur_n4m3_15nt_1t?}
+'''
