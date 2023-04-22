a294fa374764b5b6aeff532b6b8d5d2e3d2315b6
szhan21 <shunda.zhang@intel.com>
2023-04-22 10:23:48 +0800

    Solving YALM.

--- ctf/crypto/YALM/dec.py
+++ ctf/crypto/YALM/dec.py
@@ -1,8 +1,8 @@
 from pwn import *
 
-#context.log_level = 'debug'
+context.log_level = 'debug'
 
-c = 0x8ce7a727c4a70470aa3d6b872f82ef26c8ff5c7820d5790aa53e9dbd1d9f328c8847b268813c9c1bca54c3a892c9a848e95f37bb3c467971af3a29a2ea706dde662caa595728ff094b6c3c66bdddc6733f428c5b80ef81c0dbfa7f4419f08cc6ce7cde30df2004ff8037baf3647cf1813c577ca1303fb92f319418e3ed4f36dc49d33e7d92471a53ae2c029cdfa2b9034a6cb8f3b468f9154a6755aff13d923bd7e6d49d2e8db3b34b6135675d1a11236e7c8641716c54fe91dc26677200232baae8a9d5293109d73336f239d9a8905c7a1b81aec57d3df55f3302c0cddbcd742ea302c1157fa7b1138b36acd82cd73142b4fbf26099153616cd0d244dea2e1c
+#c = 0x8ce7a727c4a70470aa3d6b872f82ef26c8ff5c7820d5790aa53e9dbd1d9f328c8847b268813c9c1bca54c3a892c9a848e95f37bb3c467971af3a29a2ea706dde662caa595728ff094b6c3c66bdddc6733f428c5b80ef81c0dbfa7f4419f08cc6ce7cde30df2004ff8037baf3647cf1813c577ca1303fb92f319418e3ed4f36dc49d33e7d92471a53ae2c029cdfa2b9034a6cb8f3b468f9154a6755aff13d923bd7e6d49d2e8db3b34b6135675d1a11236e7c8641716c54fe91dc26677200232baae8a9d5293109d73336f239d9a8905c7a1b81aec57d3df55f3302c0cddbcd742ea302c1157fa7b1138b36acd82cd73142b4fbf26099153616cd0d244dea2e1c
 
 ip, port = '161.35.36.167', 31765
 io = remote(ip, port)
@@ -27,9 +27,13 @@ while low <= high:
 
 	if detect(mid-1) and detect(mid):
 		low = mid + 1
+		print mid
+		print 'Up'
 
-	elif not detect(mid-1):
+	elif not detect(mid-1) and not detect(mid):
 		high = mid - 1
+		print mid
+		print 'Down'
 
 	elif detect(mid-1) and not detect(mid):
 		print mid
