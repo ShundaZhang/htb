bbfa6c2441e093ab880d3ae21fbbd6da678724b8
szhan21 <shunda.zhang@intel.com>
2023-04-28 10:19:30 +0800

    Solving ShamirsSecret...

--- ctf/crypto/ShamirsSecret/dec.py
+++ ctf/crypto/ShamirsSecret/dec.py
@@ -14,15 +14,31 @@ key = [1]*64
 ip,port = '167.99.197.168',30589
 io = remote(ip,port)
 
-io.recvuntil('>')
-io.sendline('2')
-io.recvuntil('Input message as hex:')
-io.sendline('00')  #any even number should be OK, observe the result, odd -> even is not in key
+while sum(key) > 32:
+	io.recvuntil('>')
+	io.sendline('2')
+	io.recvuntil('Input message as hex:')
+	io.sendline('00')  #any even number should be OK, observe the result, odd -> even is not in key
 
-for _ in range(64):
+	for i in range(64):
+		buf = io.recvline().strip()
+		#print(buf)
+		x,y = buf.decode().split(', ')
+		x = int(x[1:])
+		y = int(y[:-1])
+		if x%2 == 0 and y%2 != 0:
+			key[i] = 0
+
+print(key)
+io.recvuntil('>')
+io.sendline('1')
+
+flag = [[]]
+for i in range(64):
 	buf = io.recvline().strip()
 	#print(buf)
-	x,y = buf.decode().split(', ')
-	x = int(x[1:])
-	y = int(y[:-1])
-	print(x,y)
+	if key[i]:
+		x,y = buf.decode().split(', ')
+		x = int(x[1:])
+		y = int(y[:-1])
+		flag.append([x,y])
