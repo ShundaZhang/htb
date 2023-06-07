b6c542a0ecaad2bf413c5fa581a8e6f4ad18da58
szhan21 <shunda.zhang@intel.com>
2023-06-07 13:34:04 +0800

    Solved BitsNBytes!

--- ctf/misc/BitsNBytes/dec.py
+++ ctf/misc/BitsNBytes/dec.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from Crypto.Util.number import long_to_bytes
 
 png1 = 'intercepted.png'
 png0 = 'original.png'
@@ -9,12 +10,22 @@ image0 = Image.open(png0)
 w, h = image1.size
 #print(w, h)
 
+index = []
 
 for i in range(w):
 	for j in range(h):
 		p1 = image1.getpixel((i, j))
 		p0 = image0.getpixel((i, j))
 		if (p1 != p0):
-			print(i, j)
+			#print(i, j)
+			index.append(j)
 
+fb = ''
+for i in range(416):
+	if i in index:
+		fb += '1'
+	else:
+		fb += '0'
 
+print long_to_bytes(int(fb, 2)).decode('base64')
+#HTB{1f_a_w00d_chuck_c0uld_chuck_w00d}
