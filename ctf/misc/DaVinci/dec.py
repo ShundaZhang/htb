4095ff2c268d2796be5fa29d6c0453102bd2ae25
szhan21 <shunda.zhang@intel.com>
2023-06-07 16:37:06 +0800

    Solved DaVinci!

---
+++ ctf/misc/DaVinci/dec.py
@@ -0,0 +1,29 @@
+'''
+steghide extract -sf Thepassword_is_the_small_name_of_the_actor_named_Hanks.jpg
+Enter passphrase: TOM
+wrote extracted data to "S3cr3t_m3ss@g3.txt".
+
+key:020e60c6a84db8c5d4c2d56a4e4fe082
+
+goto somd5.com
+
+leonardo
+
+stegoveritas Plans.jpg
+https://www.youtube.com/watch?v=jc1Nfx4c5LQ
+Guernica 3D
+
+stegoveritas monalisa.jpg
+unzip monalisa.zip
+unzip famous.zip with password leonardo
+
+got Mona.jpg
+
+steghide extract -sf Mona.jpg
+with password Guernica
+got a file: key
+
+base64 -d key |base64 -d|base64 -d
+
+HTB{M0n@_L1z@_!s_D3@D}
+'''
