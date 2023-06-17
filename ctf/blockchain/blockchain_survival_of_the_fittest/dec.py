7df2e83838e987fd12c9ea4736cfd048b29a1968
szhan21 <shunda.zhang@intel.com>
2023-06-17 14:16:34 +0800

    Solving blockchain_survival_of_the_fittest.

--- ctf/blockchain/blockchain_survival_of_the_fittest/dec.py
+++ ctf/blockchain/blockchain_survival_of_the_fittest/dec.py
@@ -1,1 +1,15 @@
 from web3 import Web3
+from solcx import compile_files
+
+addr = "0x008775247F3098D51e4CC6d2773436a72B01a0B3"
+target = "0x14640928E43FA763FF263aD0dBe77cf33E85e3F2"
+
+compiled = compile_files(["Creature.sol"], output_values=["abi"], solc_version="0.8.13")
+abi = compiled['Creature.sol:Creature']['abi']
+
+
+w3 = Web3(Web3.HTTPProvider('http://104.248.166.174:31576'))
+print("current balance", w3.eth.get_balance(addr), "wei")
+
+contract = w3.eth.contract(address=target, abi=abi)
+
