9fbb70762fad01d7a077375fa9fe3dcad3f85df0
szhan21 <shunda.zhang@intel.com>
2023-06-17 14:18:57 +0800

    Solving blockchain_survival_of_the_fittest.

--- ctf/blockchain/blockchain_survival_of_the_fittest/dec.py
+++ ctf/blockchain/blockchain_survival_of_the_fittest/dec.py
@@ -1,6 +1,9 @@
 from web3 import Web3
 from solcx import compile_files
 
+# installs solc compiler, necessary only for first execution
+solcx.install_solc('0.8.13')
+
 addr = "0x008775247F3098D51e4CC6d2773436a72B01a0B3"
 target = "0x14640928E43FA763FF263aD0dBe77cf33E85e3F2"
 
