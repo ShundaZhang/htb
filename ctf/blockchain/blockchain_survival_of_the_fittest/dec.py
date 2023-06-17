c66149d5cf2f44d6e98c578ef9530fff9b93d148
ShundaZhang <zhangshunda@gmail.com>
2023-06-17 16:07:58 +0800

    Solving blockchain_survival_of_the_fittest.

--- ctf/blockchain/blockchain_survival_of_the_fittest/dec.py
+++ ctf/blockchain/blockchain_survival_of_the_fittest/dec.py
@@ -1,18 +1,19 @@
 from web3 import Web3
-from solcx import compile_files, install_solc
+import json
+from web3.gas_strategies.rpc import rpc_gas_price_strategy
 
-# installs solc compiler, necessary only for first execution
-install_solc('0.8.13')
+PrivateKey = "0x5351b3054b36550274478640a950191ca28632b2d76d8f1b4b067b8e3fca40ae"
+Address = "0x008775247F3098D51e4CC6d2773436a72B01a0B3"
+TargetAddress = "0x14640928E43FA763FF263aD0dBe77cf33E85e3F2"
+setupAddress =  "0x5f102976998c981fB4A5730E2AC73FD4e09cd2D7"
 
-addr = "0x008775247F3098D51e4CC6d2773436a72B01a0B3"
-target = "0x14640928E43FA763FF263aD0dBe77cf33E85e3F2"
+url = 'http://104.248.166.174:31576'
 
-compiled = compile_files(["Creature.sol"], output_values=["abi"], solc_version="0.8.13")
-abi = compiled['Creature.sol:Creature']['abi']
+w3 = Web3(Web3.HTTPProvider(url))
+#block_number = w3.eth.block_number
+#print(block_number)
 
-
-w3 = Web3(Web3.HTTPProvider('http://104.248.166.174:31576/'))
-print("current balance", w3.eth.get_balance(addr), "wei")
-
-contract = w3.eth.contract(address=target, abi=abi)
+account_address = Address
+balance = w3.eth.get_balance(account_address)
+#print(balance)
 
