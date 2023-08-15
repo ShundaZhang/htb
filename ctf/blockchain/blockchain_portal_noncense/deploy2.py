cac01d3c5fb32957f970f47281ec8ceab1954b53
ShundaZhang <zhangshunda@gmail.com>
2023-08-15 18:47:52 +0800

    Sync portal_noncense, try in vps.

--- ctf/blockchain/blockchain_portal_noncense/deploy2.py
+++ ctf/blockchain/blockchain_portal_noncense/deploy2.py
@@ -1,34 +1,37 @@
 from web3 import Web3
 from solcx import compile_files
-from web3.middleware import geth_poa_middleware
 
 # Connect to an Ethereum node
-rpc_url = "http://157.245.43.189:32252/rpc"  # Replace with your Ethereum node's URL
+rpc_url = "http://157.245.43.189:31383/rpc"  # Replace with your Ethereum node's URL
 web3 = Web3(Web3.HTTPProvider(rpc_url))
-web3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 # Replace with your private key
 private_key = "0x2de3d450f2b9f28d5640560572511a56b1133ff1f39236fc40e51c6fb55e945c"
+sender_addr = "0xaDb67e10Fa330db49e98201B4c5F19356CfA3f59"
 
-compiled = compile_files(["Contract.sol"], output_values=["bin"], solc_version="0.8.13")
+compiled = compile_files(["Contract.sol"], output_values=["abi"], solc_version="0.8.13")
+compiled_bin = compile_files(["Contract.sol"], output_values=["bin"], solc_version="0.8.13")
 
-contract_interface = compiled["Contract.sol:Contract"]
+abi = compiled["Contract.sol:Contract"]["abi"]
+bytecode = compiled_bin["Contract.sol:Contract"]["bin"]
 
 # Set the nonce value
-#nonce = web3.eth.getTransactionCount(web3.eth.accounts[0])
-nonce = 130
-# Build the transaction
-transaction = {
-    "nonce": nonce,
-    "gasPrice": web3.to_wei("20", "gwei"),
-    "gas": 2000000,
-    "to": "",
-    "data": contract_interface["bin"],
-    #"chainId": 1,  # Mainnet
-}
+nonce = web3.eth.get_transaction_count(web3.eth.accounts[0])
+#nonce = 130
+print(f'nonce = {nonce}')
+
+Incrementer = web3.eth.contract(abi=abi, bytecode=bytecode)
+construct_txn = Incrementer.constructor().build_transaction(
+    {
+        'from': sender_addr,
+	"gasPrice": web3.to_wei(50, 'gwei'),
+	"gas": 21000,
+        'nonce': nonce
+    }
+)
 
 # Sign the transaction
-signed_txn = web3.eth.account.sign_transaction(transaction, private_key)
+signed_txn = web3.eth.account.sign_transaction(construct_txn, private_key)
 
 # Send the transaction
 tx_hash = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
