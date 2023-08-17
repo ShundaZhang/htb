5aa83d6b9719896559659acc878b6002224a2510
szhan21 <shunda.zhang@intel.com>
2023-08-17 13:08:31 +0800

    ecrecovery sign test.

--- ctf/blockchain/blockchain_funds_secured/sign_test.py
+++ ctf/blockchain/blockchain_funds_secured/sign_test.py
@@ -10,14 +10,14 @@ web3 = Web3(Web3.HTTPProvider(rpc_url))
 private_key = "0x44134c9e9a818985a3d101beb99cecb3070e888e5b84273947f30039dce82517"
 
 # Addresses from address(1) to address(0xa)
-addresses = [web3.toChecksumAddress(f"0x{str(i).zfill(40)}") for i in range(1, 0xb)]
+addresses = [web3.to_checksum_address(f"0x{str(i).zfill(40)}") for i in range(1, 0xb)]
 
 # Hash of the message to be signed
 message_hash = hashlib.sha256(b"Campaign Message").digest()
 
 # Generate signatures for each address
 for address in addresses:
-    message = encode_defunct(hexstr=Web3.toHex(message_hash))
+    message = encode_defunct(hexstr=Web3.to_hex(message_hash))
     signature = web3.eth.account.sign_message(message, private_key=private_key)
 
     # Verify the signature and recover the signer's address
