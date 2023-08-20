08a0c82adb010702c36a49a16870b2b8a407ac48
Zhang, Shunda <zhangshunda@gmail.com>
2023-08-20 02:47:55 +0000

    Editing locally...

--- ctf/blockchain/blockchain_locked_and_loaded/Contract.sol
+++ ctf/blockchain/blockchain_locked_and_loaded/Contract.sol
@@ -1,10 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "./Lockers.sol"
+
 contract Contract{
 	
+	address public YOUR_CHALLENGE_CONTRACT_ADDRESS = address(0x905e79bf459D32674ead7E5ee2A6C9780C80b2c8);
+	Lockers public creature;
+	string vendor = "V3nd0r";
+	string password2 = "P455_w0rD$$&&88@!~";
+	string name = "beliefspace";
+	string owner = "WizardsScepter";
+	
+
+	function attack2() external {
+		creature.		
+	}
+
 	receive() external payable {
 		require(false);
 	}
+
 
 }
