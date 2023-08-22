cc2c3d0a8aa60a7e1f44dd32b5854c3ab6cd651c
szhan21 <shunda.zhang@intel.com>
2023-08-22 16:37:54 +0800

    Solved Marshal in the Middle.

---
+++ ctf/forensics/MarshalintheMiddle/dec.py
@@ -0,0 +1,9 @@
+'''
+https://medium.com/@andrewss112/hackthebox-marshal-in-the-middle-ad1cc184a231
+
+ssl.log -> 10.10.20.13 pastebin.com
+add the secrets.log file to the (Pre)-Master-Secret log filename
+ip.src == 10.10.20.13 && http.request.method == POST
+
+HTB{Th15_15_4_F3nD3r_Rh0d35_M0m3NT!!}
+'''
