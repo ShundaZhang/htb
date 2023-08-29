95daedd694c851fa8111b15b4032b0f10caf2415
szhan21 <shunda.zhang@intel.com>
2023-08-29 12:50:19 +0800

    Solving aliens.

--- ctf/crypto/crypto_aliens/dec.py
+++ ctf/crypto/crypto_aliens/dec.py
@@ -14,9 +14,9 @@ for c in padding_set:
 	pt = c + padding[:-1] + 'A'*15+s_char
 	io.recvuntil('Message for encryption:')
 	io.sendline(pt)
-	io.recvline()
-	ct = io.recvline()[1:-1]
-	print(ct)
+	print(io.recvall())
+	#ct = io.recvline()[1:-1]
+	#print(ct)
 	#if ct[:32] == ct[-32*3:-32*2]:
 	#	print("Ending char: "+c)
 	#	break
