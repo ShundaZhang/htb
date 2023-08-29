99767dfef8581df4f08e206681f57cc25e652197
szhan21 <shunda.zhang@intel.com>
2023-08-29 12:46:50 +0800

    Solving aliens.

--- ctf/crypto/crypto_aliens/dec.py
+++ ctf/crypto/crypto_aliens/dec.py
@@ -1,11 +1,11 @@
 from pwn import *
 
+context.log_level = 'debug'
+
 ip, port = '157.245.43.189', 31152
 io = remote(ip, port)
 
-
 padding = 'CryptoHackTheBox'
-
 padding_set = '}CryptoHackTheBx'
 
 s_char = '\xdf'
@@ -14,8 +14,7 @@ for c in padding_set:
 	pt = c + padding[:-1] + 'A'*15+s_char
 	io.recvuntil('Message for encryption:')
 	io.sendline(pt)
-	ct = io.recvline()
-	ct = ct[1:-1]
+	ct = io.recvline()[1:-1]
 	if ct[:32] == ct[-32*3:-32*2]:
 		print("Ending char: "+c)
 		break
