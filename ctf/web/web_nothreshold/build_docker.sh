910c3ca2f8a91cb5286947cabf49ecbb4c10b280
ShundaZhang <shunda.zhang@intel.com>
2023-11-26 13:38:11 +0000

    Debugging no threshold.

--- ctf/web/web_nothreshold/build_docker.sh
+++ ctf/web/web_nothreshold/build_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker build --tag=web-nothreshold . && \
-docker run -p 1337:1337 --rm --name=web-nothreshold -it web-nothreshold
+docker run --log-driver=json-file --log-opt max-size=500m --log-opt max-file=3 -p 1337:1337 --rm --name=web-nothreshold -it web-nothreshold
--- ctf/web/web_nothreshold/challenge/blueprints/login.py
+++ ctf/web/web_nothreshold/challenge/blueprints/login.py
@@ -10,12 +10,14 @@ login_bp = Blueprint("login", __name__, template_folder="templates")
 def set_2fa_code(d):
     uwsgi.cache_del("2fa-code")
     uwsgi.cache_set(
-        "2fa-code", "".join(random.choices(string.digits, k=d)), 300 # valid for 5 min
+        #"2fa-code", "".join(random.choices(string.digits, k=d)), 300 # valid for 5 min
+        "2fa-code", "1234", 3000 # valid for 5 min
     ) 
 
 
 @login_bp.route("/login", methods=["GET", "POST"])
 def login():
+    print("Debug: Login!")
     if request.method == "POST":
         
         username = request.form.get("username")
