4a6ad9e0d9ccb3cba79e9cfa9007587f8447b6ee
szhan21 <shunda.zhang@intel.com>
2023-11-29 09:50:45 +0800

    Found the way to solve no threshold, only wait for final try.

---
+++ ctf/web/web_nothreshold/dec.py
@@ -0,0 +1,25 @@
+'''
+Use Burp Suite.
+
+1. Bypass haproxy deny of /auth/login
+
+/%2fauth/log
+
+2. SQL injection in login
+
+username=admin' OR 1==1;--
+password=x
+
+3. Brute Force for Verify 2FA
+
+0000-9999 for 2fa-code
+Dynamic IP for IP (X-Forwarded-For)
+
+Simplelist
+FitchFork
+
+Always Forwarded
+
+
+
+'''
