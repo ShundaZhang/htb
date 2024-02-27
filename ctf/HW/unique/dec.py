cf851afd1d967fc0d18c2f01a3cfb0e4d1eda96a
szhan21 <shunda.zhang@intel.com>
2024-02-27 08:56:01 +0800

    Solved unique!

---
+++ ctf/HW/unique/dec.py
@@ -0,0 +1,11 @@
+'''
+car -> CAN
+Logic2
+
+https://haxez.org/2023/08/hack-the-box-unique-writeup/
+https://www.youtube.com/watch?v=EtHGtM9CJeI
+
+Just exported the table to a .csv file, and search 'H', you can find the flag.
+
+HTB{v1n_c42_h4ck1n9_15_1337!*0^}
+'''
