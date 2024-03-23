6db6a2cb4010203e4818d6e7a6245454953beb49
szhan21 <shunda.zhang@intel.com>
2024-03-23 17:34:19 +0800

    Solved virtual mad!

---
+++ ctf/re/rev-virtuallymad/dec.py
@@ -0,0 +1,22 @@
+'''
+Pure manually code review in ghidra!
+
+02100100
+02100100
+03110001
+01121100
+04130000
+
+./virtually.mad
+Give me code to execute: 0210010002100100031100010112110004130000
+Executing 5 opcodes.
+=====
+a: 0x200
+b: 0xffffffff
+c: 0xffffffff
+d: 0x0
+flags: 0x10000000
+=====
+This is the right answer! Validate the challenge with HTB{0210010002100100031100010112110004130000}
+
+'''
