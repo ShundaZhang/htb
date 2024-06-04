d8f30e044cdb09cd979a4cfd5eb4bd67e2c04b8d
ShundaZhang <shunda.zhang@intel.com>
2024-06-04 09:56:49 +0000

    Modified the docker to let disable fuctions work.

--- ctf/web/web_jerrytok/entrypoint.sh
+++ ctf/web/web_jerrytok/entrypoint.sh
@@ -4,11 +4,11 @@
 chmod 600 /entrypoint.sh
 
 # Secure PHP Installation
-mkdir -p /etc/php82/conf.d
+mkdir -p /etc/php83/conf.d
 mkdir -p /run/apache2
 
-echo "disable_functions = exec, system, popen, proc_open, shell_exec, passthru, ini_set, putenv, pfsockopen, fsockopen, socket_create, mail" >> /etc/php82/conf.d/disablefns.ini
-echo "open_basedir = /www" >> /etc/php82/conf.d/openbdir.ini
+echo "disable_functions = exec, system, popen, proc_open, shell_exec, passthru, ini_set, putenv, pfsockopen, fsockopen, socket_create, mail" >> /etc/php83/conf.d/disablefns.ini
+echo "open_basedir = /www" >> /etc/php83/conf.d/openbdir.ini
 
 # Run supervisord
 /usr/bin/supervisord -c /etc/supervisord.conf
