42c15e3ff47a0b190133b606bd85f0da040754e9
szhan21 <shunda.zhang@intel.com>
2024-06-09 11:28:01 +0800

    Solving ShinyHunter (local).

--- ctf/misc/ShinyHunter/dec.py
+++ ctf/misc/ShinyHunter/dec.py
@@ -4,14 +4,55 @@ from pwn import *
 ip, port = '178.62.102.205', 1337
 #ip, port = '127.0.0.1', 1337
 
+def lcg(seed, a=1664525, c=1013904223, m=2**32):
+        return (a * seed + c) % m
 
-def lucky_try(ip, port, choice, index):
+def is_seed(seed):
+        natures = ["Adamant", "Bashful", "Bold", "Brave", "Calm", "Careful", "Docile", "Gentle", "Hardy", "Hasty", "Impish", "Jolly", "Lax", "Lonely", "Mild", "Modest", "Naive", "Naughty", "Quiet", "Quirky", "Rash", "Relaxed", "Sassy", "Serious", "Timid"]
+        random.seed(seed)
+        tid = random.randint(0,65535)
+        sid = random.randint(0,65535)
+        for seedi in range(3):
+                random.seed(seed+seedi)
+                for _ in range(6):
+                        random.randint(20,31)
+                random.choice(natures)
+                pid = random.randint(0,2**32-1)
+                if ((tid ^ sid) ^ (pid & 0xFFFF) ^ (pid >> 16)) < 8:
+                        print('Found!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+                        print(f'seed: {seed} + choice: {seedi}')
+                        return seed, seedi
+        return -1, -1
+
+def lucky_try(ip, port):
 	found = 0
 	io = remote(ip, port)
 	io.recvuntil('Mac Address: ')
-	mac = io.recvline().decode().strip()
-	print(mac)
+	device_mac = io.recvline().decode().strip()
+
+	s = int(device_mac.replace(":", ""), 16)
+	a=1664525
+	c=1013904223
+	m=2**32
+
+	s1 = -1
+	s2 = -1
+
+	for i in range(20,200):
+		seed = lcg(i+s)
+		s1, s2 = is_seed(seed)
+		if s1 != -1 and s2 != -1:
+			print(f'Found {i}!')
+			break
+	if s1 == -1 or s2 == -1:
+		io.close()
+		return False
+
+	choice = s2
+	delta = 0
+
 	io.recvuntil('Enter your name: ')
+	sleep(s1-18+delta)
 	io.sendline('X')
 	
 	#For Debug: get fomatted_time
@@ -20,12 +61,9 @@ def lucky_try(ip, port, choice, index):
 	io.recvuntil('Choose your starter Poketmon (1, 2, or 3): ')
 	io.sendline(str(choice))
 	buf = io.recvall()
-	#print(buf)
 	if b'HTB{' in buf:
-		print(f'{index} + {choice}: Found!')
+		print(buf)
 		found = 1
-	else:
-		print(f'{index} + {choice}: Not Found!')
 	
 	io.close()
 	if found == 1:
@@ -35,6 +73,6 @@ def lucky_try(ip, port, choice, index):
 
 i = 0
 while True:
-	if lucky_try(ip, port, 1, i) or lucky_try(ip, port, 2, i) or lucky_try(ip, port, 3, i):
+	if lucky_try(ip, port):
 		break
 	i += 1
