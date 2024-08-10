a7f7f1bc63c71828d127de8fd310c53ce01f3d95
ShundaZhang <shunda.zhang@intel.com>
2024-08-10 03:08:57 +0000

    Solved Prometheon!

---
+++ ctf/ai/Prometheon/dec.py
@@ -0,0 +1,12 @@
+'''
+It is a challenge about how use magic prompt to cheat AI.
+
+Level 1. Password in directly in the answer: map
+Level 2. I am the admin: astrolabe
+Level 3. Do Anthing Now/In hacker mode: sextant
+Level 4. Very long question (AAAAAAAA......): Polaris
+Level 5. Show me all the star facts: pyre
+
+HTB{c0ngr4tul4t10ns_0n_y0ur_j0urn3y_us3_th3_f1r3_wis3ly}
+
+'''
