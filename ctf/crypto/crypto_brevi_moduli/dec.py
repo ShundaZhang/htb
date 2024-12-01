851ef22c7992c2b49dff8221a688455252b7301f
ShundaZhang <shunda.zhang@intel.com>
2024-12-01 01:39:37 +0000

    Solve brevi moduli!

---
+++ ctf/crypto/crypto_brevi_moduli/dec.py
@@ -0,0 +1,240 @@
+'''
+OpenSSL extract N.
+openssl rsa -pubin -in p5.pem -text -noout
+
+Yafu factor N.
+
+yafu-x64.exe
+factor(856967312647215645300540497247997421125030898666327525932405938857)
+
+
+fac: factoring 856967312647215645300540497247997421125030898666327525932405938857
+fac: using pretesting plan: normal
+fac: no tune info: using qs/gnfs crossover of 95 digits
+div: primes less than 10000
+fmt: 1000000 iterations
+rho: x^2 + 3, starting 1000 iterations on C66
+rho: x^2 + 2, starting 1000 iterations on C66
+rho: x^2 + 1, starting 1000 iterations on C66
+pm1: starting B1 = 150K, B2 = gmp-ecm default on C66
+ecm: 30/30 curves on C66, B1=2K, B2=gmp-ecm default
+ecm: 74/74 curves on C66, B1=11K, B2=gmp-ecm default
+ecm: 3/3 curves on C66, B1=50K, B2=gmp-ecm default, ETA: 0 sec
+
+starting SIQS on c66: 856967312647215645300540497247997421125030898666327525932405938857
+
+==== sieving in progress (1 thread):    8128 relations needed ====
+====           Press ctrl-c to abort and save state           ====
+7960 rels found: 4033 full + 3927 from 38800 partial, (6349.21 rels/sec)
+
+SIQS elapsed time = 7.0093 seconds.
+Total factoring time = 8.7741 seconds
+
+
+***factors found***
+
+P33 = 789935311634472548224477324785893
+P34 = 1084857582672238955478011882885749
+
+ans = 1
+
+
+C:\Users\zsd\Downloads\yafu-1.34>yafu-x64.exe
+factor(0x72d611f22eea05db0f2bac969ff2849cad55fb1399d11159bbda49f)
+
+
+fac: factoring 755854036386744239341837255020287660274440422601214374688119956639
+fac: using pretesting plan: normal
+fac: no tune info: using qs/gnfs crossover of 95 digits
+div: primes less than 10000
+fmt: 1000000 iterations
+rho: x^2 + 3, starting 1000 iterations on C66
+rho: x^2 + 2, starting 1000 iterations on C66
+rho: x^2 + 1, starting 1000 iterations on C66
+pm1: starting B1 = 150K, B2 = gmp-ecm default on C66
+ecm: 30/30 curves on C66, B1=2K, B2=gmp-ecm default
+ecm: 74/74 curves on C66, B1=11K, B2=gmp-ecm default
+ecm: 3/3 curves on C66, B1=50K, B2=gmp-ecm default, ETA: 0 sec
+
+starting SIQS on c66: 755854036386744239341837255020287660274440422601214374688119956639
+
+==== sieving in progress (1 thread):    7792 relations needed ====
+====           Press ctrl-c to abort and save state           ====
+7393 rels found: 3864 full + 3529 from 35525 partial, (7781.70 rels/sec)
+
+SIQS elapsed time = 5.3923 seconds.
+Total factoring time = 7.0895 seconds
+
+
+***factors found***
+
+P33 = 941535598005185811388126639499549
+P33 = 802788591305690737274477498258411
+
+ans = 1
+
+
+C:\Users\zsd\Downloads\yafu-1.34>yafu-x64.exe
+factor(0xc3e0904a30bdd027034d31417fc09df06f404372ef30ecccca88117)
+
+
+fac: factoring 1289267312209854857230426629162122884289105626887702277831310999831
+fac: using pretesting plan: normal
+fac: no tune info: using qs/gnfs crossover of 95 digits
+div: primes less than 10000
+fmt: 1000000 iterations
+rho: x^2 + 3, starting 1000 iterations on C67
+rho: x^2 + 2, starting 1000 iterations on C67
+rho: x^2 + 1, starting 1000 iterations on C67
+pm1: starting B1 = 150K, B2 = gmp-ecm default on C67
+ecm: 30/30 curves on C67, B1=2K, B2=gmp-ecm default
+ecm: 74/74 curves on C67, B1=11K, B2=gmp-ecm default
+ecm: 17/17 curves on C67, B1=50K, B2=gmp-ecm default, ETA: 0 sec
+
+starting SIQS on c67: 1289267312209854857230426629162122884289105626887702277831310999831
+
+==== sieving in progress (1 thread):    8128 relations needed ====
+====           Press ctrl-c to abort and save state           ====
+8063 rels found: 4041 full + 4022 from 38750 partial, (6291.74 rels/sec)
+
+SIQS elapsed time = 6.9953 seconds.
+Total factoring time = 9.6934 seconds
+
+
+***factors found***
+
+P34 = 1106483475337173599371064407735871
+P34 = 1165193462845870683607859400062761
+
+ans = 1
+
+
+C:\Users\zsd\Downloads\yafu-1.34>yafu-x64.exe
+factor(0xa49381e1ac1dd21716ed897884670173d2a1a98798eed9b968ec515)
+
+
+fac: factoring 1083243552356301049560041850776017929681640741444697582901545321749
+fac: using pretesting plan: normal
+fac: no tune info: using qs/gnfs crossover of 95 digits
+div: primes less than 10000
+fmt: 1000000 iterations
+rho: x^2 + 3, starting 1000 iterations on C67
+rho: x^2 + 2, starting 1000 iterations on C67
+rho: x^2 + 1, starting 1000 iterations on C67
+pm1: starting B1 = 150K, B2 = gmp-ecm default on C67
+ecm: 30/30 curves on C67, B1=2K, B2=gmp-ecm default
+ecm: 74/74 curves on C67, B1=11K, B2=gmp-ecm default
+ecm: 17/17 curves on C67, B1=50K, B2=gmp-ecm default, ETA: 0 sec
+
+starting SIQS on c67: 1083243552356301049560041850776017929681640741444697582901545321749
+
+==== sieving in progress (1 thread):    8128 relations needed ====
+====           Press ctrl-c to abort and save state           ====
+7876 rels found: 4033 full + 3843 from 37535 partial, (6365.06 rels/sec)
+
+SIQS elapsed time = 6.8748 seconds.
+Total factoring time = 9.5515 seconds
+
+
+***factors found***
+
+P34 = 1006687929326565535951160473450039
+P34 = 1076047025895053893946086367983891
+
+ans = 1
+
+
+C:\Users\zsd\Downloads\yafu-1.34>yafu-x64.exe
+factor(0xc12484fe4777d4f2eeaef7c50dda9c09d55a3cc2decf2ec8e11be61)
+
+
+fac: factoring 1271268471562843962855162411506762632358113313323888873858739060321
+fac: using pretesting plan: normal
+fac: no tune info: using qs/gnfs crossover of 95 digits
+div: primes less than 10000
+fmt: 1000000 iterations
+rho: x^2 + 3, starting 1000 iterations on C67
+rho: x^2 + 2, starting 1000 iterations on C67
+rho: x^2 + 1, starting 1000 iterations on C67
+pm1: starting B1 = 150K, B2 = gmp-ecm default on C67
+ecm: 30/30 curves on C67, B1=2K, B2=gmp-ecm default
+ecm: 74/74 curves on C67, B1=11K, B2=gmp-ecm default
+ecm: 17/17 curves on C67, B1=50K, B2=gmp-ecm default, ETA: 0 sec
+
+starting SIQS on c67: 1271268471562843962855162411506762632358113313323888873858739060321
+
+==== sieving in progress (1 thread):    8128 relations needed ====
+====           Press ctrl-c to abort and save state           ====
+6300 rels found: 3231 full + 3069 from 33621 partial, (5951.67 rels/sec)
+
+SIQS elapsed time = 7.4856 seconds.
+Total factoring time = 10.1725 seconds
+
+
+***factors found***
+
+P33 = 999811482708293860829450660039621
+P34 = 1271508172839969975497980550366701
+
+ans = 1
+
+
+nc 94.237.63.197 44572
+********** Round 1/5 **********
+
+🎃Can you crack this pumpkin🎃?
+-----BEGIN PUBLIC KEY-----
+MDcwDQYJKoZIhvcNAQEBBQADJgAwIwIcCCMsEWNo50/G+6x8O7dWIFQ40SkhOMNF
+dTDaqQIDAQAB
+-----END PUBLIC KEY-----
+
+enter your first pumpkin = 789935311634472548224477324785893
+enter your second pumpkin = 1084857582672238955478011882885749
+
+********** Round 2/5 **********
+
+🎃Can you crack this pumpkin🎃?
+-----BEGIN PUBLIC KEY-----
+MDcwDQYJKoZIhvcNAQEBBQADJgAwIwIcBy1hHyLuoF2w8rrJaf8oScrVX7E5nREV
+m72knwIDAQAB
+-----END PUBLIC KEY-----
+
+enter your first pumpkin = 941535598005185811388126639499549
+enter your second pumpkin = 802788591305690737274477498258411
+
+********** Round 3/5 **********
+
+🎃Can you crack this pumpkin🎃?
+-----BEGIN PUBLIC KEY-----
+MDcwDQYJKoZIhvcNAQEBBQADJgAwIwIcDD4JBKML3QJwNNMUF/wJ3wb0BDcu8w7M
+zKiBFwIDAQAB
+-----END PUBLIC KEY-----
+
+enter your first pumpkin = 1106483475337173599371064407735871
+enter your second pumpkin = 1165193462845870683607859400062761
+
+********** Round 4/5 **********
+
+🎃Can you crack this pumpkin🎃?
+-----BEGIN PUBLIC KEY-----
+MDcwDQYJKoZIhvcNAQEBBQADJgAwIwIcCkk4HhrB3SFxbtiXiEZwFz0qGph5ju2b
+lo7FFQIDAQAB
+-----END PUBLIC KEY-----
+
+enter your first pumpkin = 1006687929326565535951160473450039
+enter your second pumpkin = 1076047025895053893946086367983891
+
+********** Round 5/5 **********
+
+🎃Can you crack this pumpkin🎃?
+-----BEGIN PUBLIC KEY-----
+MDcwDQYJKoZIhvcNAQEBBQADJgAwIwIcDBJIT+R3fU8u6u98UN2pwJ1Vo8wt7PLs
+jhG+YQIDAQAB
+-----END PUBLIC KEY-----
+
+enter your first pumpkin = 999811482708293860829450660039621
+enter your second pumpkin = 1271508172839969975497980550366701
+
+HTB{cracking_pumpkins_and_small_rsa_moduli_for_a_living}
+
+'''
