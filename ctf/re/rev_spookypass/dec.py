6871d8132e5d60830446c2de108884f3ed6ddb1f
ShundaZhang <shunda.zhang@intel.com>
2024-12-15 02:30:25 +0000

    Solved spookypass.

---
+++ ctf/re/rev_spookypass/dec.py
@@ -0,0 +1,10 @@
+'''
+ghidra
+
+./pass
+Welcome to the SPOOKIEST party of the year.
+Before we let you in, you'll need to give us the password: s3cr3t_p455_f0r_gh05t5_4nd_gh0ul5
+Welcome inside!
+HTB{un0bfu5c4t3d_5tr1ng5}
+
+'''
