9e6223c0d7602fe7c333cc5524412c8f0c7913e2
ShundaZhang <shunda.zhang@intel.com>
2024-12-15 13:25:10 +0000

    Solved locked away.

---
+++ ctf/misc/misc_locked_away/dec.py
@@ -0,0 +1,15 @@
+'''
+keywrods -- to help searching
+python sandbox break
+python sandbox escape
+python jail break
+python jail escape
+
+https://motasemhamdan.medium.com/hackthebox-locked-away-python-ctf-writeups-1108ac87b898
+
+The chest lies waiting... blacklist.clear()
+The chest lies waiting... open_chest()
+HTB{bL4cKl1sT?_bUt_tH4t'5_t0o_3asY}
+The chest lies waiting... globals().get(bytes((111, 112, 101, 110, 95, 99, 104, 101, 115, 116)).decode())()
+HTB{bL4cKl1sT?_bUt_tH4t'5_t0o_3asY}
+'''
